cfd9f2a3e06765eba7675e23caa9d81e2b24e1f6
houhaoyu <houhaoyu@huawei.com>
2022-03-07 09:44:33 +0800

    fixed 38752e5 from https://gitee.com/houhaoyu/developtools_ace-ets2bundle/pulls/267 houhaoyu@huawei.com
    fix debugline
    
    Signed-off-by: houhaoyu <houhaoyu@huawei.com>
    Change-Id: I3d1af6f4d781199c55c3bdb64a3085f34c48601a

--- compiler/server/build_pipe_server.js
+++ compiler/server/build_pipe_server.js
@@ -22,6 +22,12 @@ const WebSocketServer = WebSocket.Server;
 
 let pluginSocket = '';
 
+let supplement = {
+  isAcceleratePreview: false,
+  line: 0,
+  column: 0
+}
+
 const pluginCommandChannelMessageHandlers = {
   'compileComponent': handlePluginCompileComponent,
   'default': () => {}
@@ -50,14 +56,35 @@ function handlePluginCommand(jsonData) {
 
 function handlePluginCompileComponent(jsonData) {
   const receivedMsg = jsonData;
-  const sourceNode = ts.createSourceFile
-    ('preview.ts', receivedMsg.data.script, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
+  const sourceNode = ts.createSourceFile('preview.ts', receivedMsg.data.script,
+    ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
   const previewStatements = [];
-  processComponentChild(sourceNode, previewStatements, []);
+  const log = [];
+  supplement = {
+    isAcceleratePreview: true,
+    line: parseInt(JSON.parse(receivedMsg.data.offset).line),
+    column: parseInt(JSON.parse(receivedMsg.data.offset).column)
+  }
+  processComponentChild(sourceNode, previewStatements, log, supplement);
+  supplement.isAcceleratePreview = false;
   const newSource = ts.factory.updateSourceFile(sourceNode, previewStatements);
   const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
   const result = printer.printNode(ts.EmitHint.Unspecified, newSource, newSource);
   receivedMsg.data.script = ts.transpileModule(result, {}).outputText;
+  if (receivedMsg.data.offset) {
+    for (let i = 0; i < log.length; i++) {
+      let line = parseInt(newSource.getLineAndCharacterOfPosition(log[i].pos).line);
+      let column = parseInt(newSource.getLineAndCharacterOfPosition(log[i].pos).character);
+      if (line === 0) {
+        log[i].line = parseInt(JSON.parse(receivedMsg.data.offset).line);
+        log[i].column = parseInt(JSON.parse(receivedMsg.data.offset).column) + column;
+      } else {
+        log[i].line = parseInt(JSON.parse(receivedMsg.data.offset).line) + line;
+        log[i].column = column;
+      }
+    }
+  }
+  receivedMsg.data.log = log;
   if (pluginSocket.readyState === WebSocket.OPEN){
     responseToPlugin(receivedMsg);
   }
