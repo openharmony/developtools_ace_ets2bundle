472e8361af8e2d2879457d460fa55af8ca64deb3
jiangbo <jiangbo91@huawei.com>
2022-06-20 19:38:10 +0800

    jiangbo91@huawei.com
    fix system module name case sensitive.
    
    Signed-off-by: jiangbo <jiangbo91@huawei.com>

--- compiler/src/ets_checker.ts
+++ compiler/src/ets_checker.ts
@@ -17,7 +17,10 @@ import fs from 'fs';
 import path from 'path';
 import * as ts from 'typescript';
 
-import { projectConfig } from '../main';
+import {
+  projectConfig,
+  systemModules
+} from '../main';
 import {
   processSystemApi,
   preprocessExtend,
@@ -124,7 +127,7 @@ function resolveModuleNames(moduleNames: string[], containingFile: string): ts.R
       resolvedModules.push(result.resolvedModule);
     } else if (/^@(system|ohos)/.test(moduleName.trim())) {
       const modulePath: string = path.resolve(__dirname, '../../../api', moduleName + '.d.ts');
-      if (ts.sys.fileExists(modulePath)) {
+      if (systemModules.includes(moduleName + '.d.ts') && ts.sys.fileExists(modulePath)) {
         resolvedModules.push(getResolveModule(modulePath, '.d.ts'));
       } else {
         resolvedModules.push(null);
