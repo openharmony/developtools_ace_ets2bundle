db0ad8d230ae09fc78de1dc9556353e2b3d29918
jiangbo <jiangbo91@huawei.com>
2022-06-30 18:38:08 +0800

    jiangbo91@huawei.com
    代码规范化
    
    Signed-off-by: jiangbo <jiangbo91@huawei.com>

--- compiler/src/process_component_build.ts
+++ compiler/src/process_component_build.ts
@@ -1284,7 +1284,7 @@ export function getName(node: ts.ExpressionStatement | ts.Expression): string {
       name = temp.escapedText.toString();
       break;
     } else if (ts.isPropertyAccessExpression(temp) && temp.name && ts.isIdentifier(temp.name) &&
-    isCustomAttributes(temp)) {
+      isCustomAttributes(temp)) {
       name = temp.name.escapedText.toString();
       break;
     }
