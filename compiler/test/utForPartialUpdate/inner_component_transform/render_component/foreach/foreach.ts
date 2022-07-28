76a901a5ba719e4264fe270d580fab9e6e40e53b
laibo102 <laibo2@huawei.com>
2022-07-28 10:30:33 +0800

    Update: Foreach & LazyForeach component
    Signed-off-by: laibo102 <laibo2@huawei.com>
    Change-Id: Iaa2e39cde53a28dfa6528eb4f3e542d6e1981264
    (cherry picked from commit 30960824aff42fcecf3b3e36806137637b86e9c1)
    Signed-off-by: laibo102 <laibo2@huawei.com>

---
+++ compiler/test/utForPartialUpdate/inner_component_transform/render_component/foreach/foreach.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+exports.source = `
+@Entry
+@Component
+struct ParentView {
+  @State arr: string[] = ['1', '2', '3'];
+  build() {
+    List() {
+      ForEach(this.arr,
+        item => {
+          Text(item)
+        },
+        item => item.toString()
+      )
+    }
+  }
+}
+`
+exports.expectResult =
+`class ParentView extends View {
+    constructor(parent, params) {
+        super(parent);
+        this.__arr = new ObservedPropertyObject(['1', '2', '3'], this, "arr");
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+        if (params.arr !== undefined) {
+            this.arr = params.arr;
+        }
+    }
+    setStateSourcePropertiesUnchanged() {
+        this.__arr.SetPropertyUnchanged();
+    }
+    setOneWaySyncPropertiesUnchanged() {
+    }
+    setTwoWaySyncPropertiesUnchanged() {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+        this.__arr.purgeDependencyOnElmtId(rmElmtId);
+    }
+    aboutToBeDeleted() {
+        this.__arr.aboutToBeDeleted();
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    get arr() {
+        return this.__arr.get();
+    }
+    set arr(newValue) {
+        this.__arr.set(newValue);
+    }
+    render() {
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            List.create();
+            if (!isInitialRender) {
+                List.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            ForEach.create();
+            const forEachItemGenFunction = _item => {
+                const item = _item;
+                this.observeComponentCreation((elmtId, isInitialRender) => {
+                    ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+                    Text.create(item);
+                    if (!isInitialRender) {
+                        Text.pop();
+                    }
+                    ViewStackProcessor.StopGetAccessRecording();
+                });
+                Text.pop();
+            };
+            const forEachItemIdFunc = item => item.toString();
+            this.forEachUpdateFunction(elmtId, this.arr, forEachItemIdFunc, forEachItemGenFunction);
+            if (!isInitialRender) {
+                ForEach.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        ForEach.pop();
+        List.pop();
+    }
+    rerender() {
+        this.__arr.markDependentElementsDirty(this);
+        this.updateDirtyElements();
+    }
+}
+loadDocument(new ParentView("1", undefined, {}));
+`
