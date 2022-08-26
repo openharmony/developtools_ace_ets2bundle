52056ce92fa821c79cbdd1e3e468bf71563bf47c
zhangrengao <zhangrengao1@huawei.com>
2022-08-26 22:12:03 +0800

    Add clean webpack plugin
    Signed-off-by: zhangrengao <zhangrengao1@huawei.com>
    Change-Id: I191dfdc7a5b013ab8b22f8a2f8b16e698a8b02bb

--- compiler/src/gen_abc_plugin.ts
+++ compiler/src/gen_abc_plugin.ts
@@ -137,7 +137,6 @@ export class GenAbcPlugin {
       return;
     }
 
-    removeDir(output);
     if (projectConfig.compileMode === ESMODULE) {
       removeDir(projectConfig.nodeModulesPath);
     }
