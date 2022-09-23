247b69b2783c16ce715107e0b491a1d0ea0b00dd
lizhouze <lizhouze@huawei.com>
2022-09-23 14:39:52 +0800

    lizhouze@huawei.com
    Signed-off-by: lizhouze <lizhouze@huawei.com>

--- compiler/main.js
+++ compiler/main.js
@@ -306,7 +306,15 @@ function readWorkerFile(isPreview) {
         }
         const relativePath = path.relative(projectConfig.projectPath, worker);
         if (filterWorker(relativePath)) {
-          workerFileEntry[relativePath.replace(/\.(ts|js)$/, '').replace(/\\/g, '/')] = worker;
+          const workerKey = relativePath.replace(/\.(ts|js)$/, '').replace(/\\/g, '/');
+          if (workerFileEntry[workerKey]) {
+            throw Error(
+              '\u001b[31m ERROR: The worker file cannot use the same file name: ' +
+              workerKey + '. \u001b[39m'
+            ).message;
+          } else {
+            workerFileEntry[workerKey] = worker;
+          }
         }
       });
       return workerFileEntry;
