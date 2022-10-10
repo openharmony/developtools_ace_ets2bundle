10920758b5909f93828e91e4aaf03bfc18330dfe
yfwang6 <wangyongfei6@huawei.com>
2022-10-10 20:54:23 +0800

    wangyongfei6@huawei.com
    fix pagetransition on partial update mode
    
    Signed-off-by: yfwang6 <wangyongfei6@huawei.com>
    Change-Id: Id771d5ca158c60dea41cbaf28d586cd70517a72a

--- compiler/src/process_component_build.ts
+++ compiler/src/process_component_build.ts
@@ -156,12 +156,21 @@ export function processComponentBlock(node: ts.Block, isLazy: boolean, log: LogI
     newStatements.unshift(createRenderingInProgress(true));
   }
   if (isTransition) {
-    newStatements.unshift(ts.factory.createExpressionStatement(
-      createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
-        ts.factory.createIdentifier(COMPONENT_CREATE_FUNCTION), null)));
-    newStatements.push(ts.factory.createExpressionStatement(
-      createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
-        ts.factory.createIdentifier(COMPONENT_POP_FUNCTION), null)));
+    if (!partialUpdateConfig.partialUpdateMode) {
+      newStatements.unshift(ts.factory.createExpressionStatement(
+        createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
+          ts.factory.createIdentifier(COMPONENT_CREATE_FUNCTION), null)));
+      newStatements.push(ts.factory.createExpressionStatement(
+        createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
+          ts.factory.createIdentifier(COMPONENT_POP_FUNCTION), null)));
+    } else {
+      newStatements.unshift(createComponentCreationStatement(node, [ts.factory.createExpressionStatement(
+        createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
+          ts.factory.createIdentifier(COMPONENT_CREATE_FUNCTION), null))]));
+      newStatements.push(createComponentCreationStatement(node, [ts.factory.createExpressionStatement(
+        createFunction(ts.factory.createIdentifier(COMPONENT_TRANSITION_NAME),
+          ts.factory.createIdentifier(COMPONENT_POP_FUNCTION), null))]));
+    }
   }
   if (isLazy && !partialUpdateConfig.partialUpdateMode) {
     newStatements.push(createRenderingInProgress(false));
