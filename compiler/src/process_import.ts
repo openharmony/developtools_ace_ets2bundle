7021acf9bc66cba92af566752886f15775c2ee1a
hufeng <hufeng20@huawei.com>
2022-12-08 17:30:09 +0800

    Add errors log when re-export a [type] without type keyword
    Signed-off-by: hufeng <hufeng20@huawei.com>
    Change-Id: I1df72c35d727666a55674018a50b3c94b3970c55

--- compiler/src/process_import.ts
+++ compiler/src/process_import.ts
@@ -96,30 +96,13 @@ isEntryPage: boolean = true, pathCollection: Set<string> = new Set()): void {
       }
     }
   }
-  if (filePath && path.extname(filePath) !== EXTNAME_ETS && !isModule(filePath)) {
-    const dirIndexPath: string = path.resolve(path.resolve(pagesDir, filePath), INDEX_ETS);
-    if (/^(\.|\.\.)\//.test(filePath) && !fs.existsSync(path.resolve(pagesDir, filePath + EXTNAME_ETS)) &&
-      fs.existsSync(dirIndexPath)) {
-      filePath = dirIndexPath;
-    } else {
-      filePath += EXTNAME_ETS;
-    }
-  }
 
   if (filePath) {
     validatorCard(log, CARD_LOG_TYPE_IMPORT, node.getStart());
   }
 
   try {
-    let fileResolvePath: string;
-    if (/^(\.|\.\.)\//.test(filePath) && filePath.indexOf(NODE_MODULES) < 0) {
-      fileResolvePath = path.resolve(pagesDir, filePath);
-    } else if (/^\//.test(filePath) && filePath.indexOf(NODE_MODULES) < 0 ||
-      fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
-      fileResolvePath = filePath;
-    } else {
-      fileResolvePath = getFileResolvePath(fileResolvePath, pagesDir, filePath, projectConfig.projectPath);
-    }
+    let fileResolvePath: string = getFileFullPath(filePath, pagesDir);
     if (fs.existsSync(fileResolvePath) && fs.statSync(fileResolvePath).isFile() &&
       !pathCollection.has(fileResolvePath)) {
       let sourceFile: ts.SourceFile;
@@ -127,12 +110,7 @@ isEntryPage: boolean = true, pathCollection: Set<string> = new Set()): void {
       if (IMPORT_FILE_ASTCACHE.has(fileResolvePath)) {
         sourceFile = IMPORT_FILE_ASTCACHE.get(fileResolvePath);
       } else {
-        const content: string = preprocessNewExtend(preprocessExtend(processSystemApi(
-          fs.readFileSync(fileResolvePath, { encoding: 'utf-8' }).replace(
-            new RegExp('\\b' + STRUCT + '\\b.+\\{', 'g'), item => {
-              return item.replace(new RegExp('\\b' + STRUCT + '\\b', 'g'), `${CLASS} `);
-            }))));
-        sourceFile = ts.createSourceFile(filePath, content, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
+        sourceFile = generateSourceFileAST(fileResolvePath, filePath);
         IMPORT_FILE_ASTCACHE[fileResolvePath] = sourceFile;
       }
       visitAllNode(sourceFile, sourceFile, defaultName, asName, path.dirname(fileResolvePath), log,
@@ -141,6 +119,16 @@ isEntryPage: boolean = true, pathCollection: Set<string> = new Set()): void {
   } catch (e) {
     // ignore
   }
+}
+
+function generateSourceFileAST(fileResolvePath: string, filePath: string): ts.SourceFile {
+  const originContent: string = fs.readFileSync(fileResolvePath, { encoding: 'utf-8' });
+  const content: string = path.extname(fileResolvePath) === EXTNAME_ETS ?
+    preprocessNewExtend(preprocessExtend(processSystemApi(originContent.replace(
+      new RegExp('\\b' + STRUCT + '\\b.+\\{', 'g'), item => {
+        return item.replace(new RegExp('\\b' + STRUCT + '\\b', 'g'), `${CLASS} `);
+      })))) : originContent;
+  return ts.createSourceFile(filePath, content, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
 }
 
 function visitAllNode(node: ts.Node, sourceFile: ts.SourceFile, defaultNameFromParent: string,
@@ -489,6 +477,42 @@ function getFileResolvePath(fileResolvePath: string, pagesDir: string, filePath:
   return fileResolvePath;
 }
 
+function addExtension(filePath: string): string {
+  if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    return "";
+  }
+
+  let extension: string = EXTNAME_ETS;
+  if (fs.existsSync(filePath + '.ts') && fs.statSync(filePath + '.ts').isFile()) {
+    extension = '.ts';
+  }
+  return extension;
+}
+
+function getFileFullPath(filePath: string, pagesDir: string): string {
+  if (filePath && path.extname(filePath) !== EXTNAME_ETS && !isModule(filePath)) {
+    const dirIndexPath: string = path.resolve(path.resolve(pagesDir, filePath), INDEX_ETS);
+    if (/^(\.|\.\.)\//.test(filePath) && !fs.existsSync(path.resolve(pagesDir, filePath + EXTNAME_ETS)) &&
+      fs.existsSync(dirIndexPath)) {
+      filePath = dirIndexPath;
+    } else {
+      filePath += addExtension(path.resolve(pagesDir, filePath));
+    }
+  }
+
+  let fileResolvePath: string;
+  if (/^(\.|\.\.)\//.test(filePath) && filePath.indexOf(NODE_MODULES) < 0) {
+    fileResolvePath = path.resolve(pagesDir, filePath);
+  } else if (/^\//.test(filePath) && filePath.indexOf(NODE_MODULES) < 0 ||
+    fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    fileResolvePath = filePath;
+  } else {
+    fileResolvePath = getFileResolvePath(fileResolvePath, pagesDir, filePath, projectConfig.projectPath);
+  }
+
+  return fileResolvePath;
+}
+
 function validateModuleName(moduleNode: ts.Identifier, log: LogInfo[], sourceFile?: ts.SourceFile,
   fileResolvePath?: string): void {
   const moduleName: string = moduleNode.escapedText.toString();
@@ -510,4 +534,325 @@ function validateModuleName(moduleNode: ts.Identifier, log: LogInfo[], sourceFil
     }
     log.push(error);
   }
+}
+
+interface ImportName {
+  name: string,
+  node: ts.Node,
+  source: string
+}
+
+function collectNonTypeMarkedReExportName(node: ts.SourceFile, pagesDir: string): Map<string, Map<string, ts.Node>> {
+  /* those cases need be validated
+   * case 1: re-export
+   *   export { externalName as localName } from './xxx'
+   * 
+   * case 2: indirect re-export nameBindings
+   *   import { externalName as localName } from './xxx'
+   *   export [type] { localName as re-exportName }
+   * 
+   * case 3: indirect re-export default
+   *   import defaultLocalName from './xxx'
+   *   export [type] { defaultLocalName as re-exportName }
+   */
+  const RE_EXPORT_NAME: Map<string, Map<string, ts.Node>> = new Map();
+  const IMPORT_AS: Map<string, ImportName> = new Map();
+  const EXPORT_LOCAL: Set<string> = new Set();
+
+  node.statements.forEach(stmt => {
+    if (ts.isImportDeclaration(stmt) && stmt.importClause && !stmt.importClause.isTypeOnly) {
+      let fileFullPath: string = getFileFullPath(stmt.moduleSpecifier.getText().replace(/'|"/g, ''), pagesDir);
+      const importClause: ts.ImportClause = stmt.importClause;
+      if (importClause.name) {
+        let localName: string = importClause.name.escapedText.toString();
+        let importName: ImportName = {name: 'default', node: stmt, source: fileFullPath};
+        IMPORT_AS.set(localName, importName);
+      }
+      if (importClause.namedBindings && ts.isNamedImports(importClause.namedBindings)) {
+        importClause.namedBindings.elements.forEach(elem => {
+          let localName: string = elem.name.escapedText.toString();
+          let importName: string = elem.propertyName ? elem.propertyName.escapedText.toString() : localName;
+          IMPORT_AS.set(localName, <ImportName>{name: importName, node: stmt, source: fileFullPath})
+        });
+      }
+    }
+
+    if (ts.isExportDeclaration(stmt)) {
+      if (stmt.moduleSpecifier && !stmt.isTypeOnly && stmt.exportClause && ts.isNamedExports(stmt.exportClause)) {
+        let fileFullPath: string = getFileFullPath(stmt.moduleSpecifier.getText().replace(/'|"/g, ''), pagesDir);
+        stmt.exportClause.elements.forEach(elem => {
+          let importName: string = elem.propertyName ? elem.propertyName.escapedText.toString() :
+                                   elem.name.escapedText.toString();
+          if (RE_EXPORT_NAME.has(fileFullPath)) {
+            RE_EXPORT_NAME.get(fileFullPath).set(importName, stmt);
+          } else {
+            RE_EXPORT_NAME.set(fileFullPath, (new Map<string, ts.Node>()).set(importName, stmt));
+          }
+        })
+      }
+      if (!stmt.moduleSpecifier && stmt.exportClause && ts.isNamedExports(stmt.exportClause)) {
+        stmt.exportClause.elements.forEach(elem => {
+          let localName: string = elem.propertyName ? elem.propertyName.escapedText.toString() :
+                                  elem.name.escapedText.toString();
+          EXPORT_LOCAL.add(localName);
+        });
+      }
+    }
+  });
+
+  EXPORT_LOCAL.forEach(local => {
+    if (IMPORT_AS.has(local)) {
+      let importName: ImportName = IMPORT_AS.get(local);
+      if (RE_EXPORT_NAME.has(importName.source)) {
+        RE_EXPORT_NAME.get(importName.source).set(importName.name, importName.node);
+      } else {
+        RE_EXPORT_NAME.set(importName.source, (new Map<string, ts.Node>()).set(importName.name, importName.node));
+      }
+    }
+  });
+
+  return RE_EXPORT_NAME;
+}
+
+function processTypeImportDecl(node: ts.ImportDeclaration, localTypeNames: Set<string>): void {
+  if (node.importClause && node.importClause.isTypeOnly) {
+    // import type T from ...
+    if (node.importClause.name) {
+      localTypeNames.add(node.importClause.name.escapedText.toString());
+    }
+    // import type * as T from ...
+    if (node.importClause.namedBindings && ts.isNamespaceImport(node.importClause.namedBindings)) {
+      localTypeNames.add(node.importClause.namedBindings.name.escapedText.toString());
+    }
+    // import type { e_T as T } from ...
+    if (node.importClause.namedBindings && ts.isNamedImports(node.importClause.namedBindings)) {
+      node.importClause.namedBindings.elements.forEach((elem: any) => {
+        localTypeNames.add(elem.name.escapedText.toString());
+      });
+    }
+  }
+}
+
+function processExportDecl(node: ts.ExportDeclaration, typeExportNames: Set<string>,
+                           exportAs: Map<string, string>): void {
+  if (node.isTypeOnly) {
+    if (node.moduleSpecifier && node.exportClause) {
+      // export type * as T from ...
+      if (ts.isNamespaceExport(node.exportClause)) {
+        typeExportNames.add(node.exportClause.name.escapedText.toString());
+      }
+      // export type { e_T as T } from ...
+      if (ts.isNamedExports(node.exportClause)) {
+        node.exportClause.elements.forEach((elem: any) => {
+          typeExportNames.add(elem.name.escapedText.toString());
+          
+        })
+      }
+    }
+    // export type { e_T as T }
+    if (!node.moduleSpecifier && node.exportClause && ts.isNamedExports(node.exportClause)) {
+      node.exportClause.elements.forEach((elem: any) => {
+        typeExportNames.add(elem.name.escapedText.toString());
+      });
+    }
+  } else {
+    // export { e_T as T }
+    if (!node.moduleSpecifier && node.exportClause && ts.isNamedExports(node.exportClause)) {
+      node.exportClause.elements.forEach((elem: any) => {
+        let exportName: string = elem.name.escapedText.toString();
+        let localName: string = elem.propertyName ? elem.propertyName.escapedText.toString() : exportName;
+        exportAs.set(localName, exportName);
+      });
+    }
+  }
+}
+
+function processEnum(node: ts.EnumDeclaration, localTypeNames: Set<string>, typeExportNames: Set<string>): void {
+  // 1. export const enum E{}
+  // 2. const enum E{} export {E as EE}
+  // 3. export const enum E{} export {E as EE}
+  let hasConst: boolean = false, hasExport: boolean = false;
+  node.modifiers && node.modifiers.forEach(m => {
+    if (m.kind == ts.SyntaxKind.ConstKeyword) {
+      hasConst = true;
+    }
+    if (m.kind == ts.SyntaxKind.ExportKeyword) {
+      hasExport = true;
+    }
+  });
+  if (!hasConst) {
+    return;
+  }
+  if (hasExport) {
+    typeExportNames.add(node.name.escapedText.toString());
+  }
+  localTypeNames.add(node.name.escapedText.toString());
+}
+
+function processInterfaceAndTypeAlias(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration,
+                                      localTypeNames: Set<string>, typeExportNames: Set<string>): void {
+  let hasDefault: boolean = false, hasExport: boolean = false;
+  node.modifiers && node.modifiers.forEach(m => {
+    if (m.kind == ts.SyntaxKind.DefaultKeyword) {
+      hasDefault = true;
+    }
+    if (m.kind == ts.SyntaxKind.ExportKeyword) {
+      hasExport = true;
+    }
+  });
+  localTypeNames.add(node.name.escapedText.toString());
+
+  if (hasExport) {
+    let exportName = hasDefault ? 'default' : node.name.escapedText.toString();
+    typeExportNames.add(exportName);
+  }
+}
+
+function checkTypeModuleDeclIsType(node: ts.ModuleDeclaration): boolean {
+  if (ts.isIdentifier(node.name) && node.body && ts.isModuleBlock(node.body)) {
+    for (let idx = 0; idx < node.body.statements.length; idx++) {
+      let stmt: ts.Statement = node.body.statements[idx];
+      if (ts.isModuleDeclaration(stmt)) {
+        if (!checkTypeModuleDeclIsType(<ts.ModuleDeclaration>stmt)) {
+          return false;
+        }
+      } else if (ts.isEnumDeclaration(stmt)) {
+        let hasConst: boolean = false;
+        stmt.modifiers && stmt.modifiers.forEach(m => {
+          if (m.kind == ts.SyntaxKind.ConstKeyword) {
+            hasConst = true;
+          }
+        });
+        if (!hasConst) {
+          return false;
+        }
+      } else if (ts.isImportEqualsDeclaration(stmt)) {
+        let hasExport: boolean = false;
+        stmt.modifiers && stmt.modifiers.forEach(m => {
+          if (m.kind == ts.SyntaxKind.ExportKeyword) {
+            hasExport = true;
+          }
+        });
+        if (hasExport) {
+          return false;
+        }
+      } else if (!ts.isInterfaceDeclaration(stmt) || !ts.isTypeAliasDeclaration(stmt)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function processNamespace(node: ts.ModuleDeclaration, localTypeNames: Set<string>, typeExportNames: Set<string>): void {
+  if (ts.isIdentifier(node.name) && node.body && ts.isModuleBlock(node.body)) {
+    if (!checkTypeModuleDeclIsType(<ts.ModuleDeclaration>node)) {
+      return;
+    }
+
+    let hasExport: boolean = false;
+    node.modifiers && node.modifiers.forEach(m => {
+      if (m.kind == ts.SyntaxKind.ExportKeyword) {
+        hasExport = true;
+      }
+    });
+    if (hasExport) {
+      typeExportNames.add(node.name.escapedText.toString());
+    }
+    localTypeNames.add(node.name.escapedText.toString());
+  }
+}
+
+function addErrorLogIfReExportType(sourceFile: ts.SourceFile, log: LogInfo[], localTypeNames: Set<string>,
+  typeExportNames: Set<string>, exportAs: Map<string, string>, exportNames: Map<string, ts.Node>): void {
+  localTypeNames.forEach(localName => {
+    if (exportAs.has(localName)) {
+      typeExportNames.add(exportAs.get(localName));
+    }
+  });
+  let reExportNamesArray: Array<string> = Array.from(exportNames.keys());
+  let typeExportNamesArray: Array<string> = Array.from(typeExportNames);
+  const needWarningNames: Array<string> = reExportNamesArray.filter(name => typeExportNamesArray.includes(name));
+  needWarningNames.forEach(name => {
+    const moduleNode: ts.Node = exportNames.get(name)!;
+    const posOfNode: ts.LineAndCharacter = sourceFile.getLineAndCharacterOfPosition(moduleNode.getStart());
+    const error: LogInfo = {
+      type: LogType.ERROR,
+      message: `The re-export name '${name}' need to be marked as type. please use 'export type'`,
+      pos: moduleNode.getStart(),
+      fileName: sourceFile.fileName,
+      line: posOfNode.line + 1,
+      column: posOfNode.character + 1
+    }
+    log.push(error);
+  });
+}
+
+export function validateReExportType(node: ts.SourceFile, pagesDir: string, log: LogInfo[]): void {
+  /*
+   * those cases' name should be treat as Type 
+   * case1: 
+   *   import type {T} from ...
+   *   import type T from ...
+   *   import type * as T from ...
+   * case2:
+   *   export type {T} from ...
+   *   export type * as T from ...
+   * case3:
+   *   export interface T {}
+   *   export Type T = {}
+   * case4:
+   *   export default interface {}
+   * case5:
+   *   interface T {}
+   *   export {T}
+   */
+  const RE_EXPORT_NAME: Map<string, Map<string, ts.Node>> = collectNonTypeMarkedReExportName(node, pagesDir);
+  RE_EXPORT_NAME.forEach((exportNames: Map<string, ts.Node>, source: string) => {
+    let importFileAst: ts.SourceFile;
+    if (IMPORT_FILE_ASTCACHE.has(source)) {
+      importFileAst = IMPORT_FILE_ASTCACHE.get(source);
+    } else {
+      importFileAst = generateSourceFileAST(source, source);
+      IMPORT_FILE_ASTCACHE[source] = importFileAst;
+    }
+    const EXPORT_AS: Map<string, string> = new Map();
+    const LOCAL_TYPE_NAMES: Set<string> = new Set();
+    const TYPE_EXPORT_NAMES: Set<string> = new Set();
+    importFileAst.statements.forEach(stmt => {
+      switch(stmt.kind) {
+        case ts.SyntaxKind.ImportDeclaration: {
+          processTypeImportDecl(<ts.ImportDeclaration>stmt, LOCAL_TYPE_NAMES);
+          break;
+        }
+        case ts.SyntaxKind.ExportDeclaration: {
+          processExportDecl(<ts.ExportDeclaration>stmt, TYPE_EXPORT_NAMES, EXPORT_AS);
+          break;
+        }
+        case ts.SyntaxKind.ExportAssignment: {
+          if (ts.isIdentifier((<ts.ExportAssignment>stmt).expression) && exportNames.has("default")) {
+            EXPORT_AS.set((<ts.Identifier>(<ts.ExportAssignment>stmt).expression).escapedText.toString(), "default");
+          }
+          break;
+        }
+        case ts.SyntaxKind.EnumDeclaration: {
+          processEnum(<ts.EnumDeclaration>stmt, LOCAL_TYPE_NAMES, TYPE_EXPORT_NAMES);
+          break;
+        }
+        case ts.SyntaxKind.ModuleDeclaration: {
+          processNamespace(<ts.ModuleDeclaration>stmt, LOCAL_TYPE_NAMES, TYPE_EXPORT_NAMES);
+          break;
+        }
+        case ts.SyntaxKind.InterfaceDeclaration:
+        case ts.SyntaxKind.TypeAliasDeclaration: {
+          processInterfaceAndTypeAlias(<ts.InterfaceDeclaration|ts.TypeAliasDeclaration>stmt,
+                                        LOCAL_TYPE_NAMES, TYPE_EXPORT_NAMES);
+          break;
+        }
+        default:
+          break;
+      }
+    });
+    addErrorLogIfReExportType(node, log, LOCAL_TYPE_NAMES, TYPE_EXPORT_NAMES, EXPORT_AS, exportNames);
+  });
 }
