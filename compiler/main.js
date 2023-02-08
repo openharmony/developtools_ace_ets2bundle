96c5476b6734056d23d04c2e875723cf5b865460
raul <laoyitong@huawei.com>
2023-02-08 16:36:52 +0800

    seprate card and app into two abc
    Signed-off-by: raul <laoyitong@huawei.com>
    Change-Id: I21b40b96d7a14115b79efaaed4101540c9b0c4e3

--- compiler/main.js
+++ compiler/main.js
@@ -92,6 +92,7 @@ function initProjectConfig(projectConfig) {
   projectConfig.obfuscateHarType = projectConfig.obfuscateHarType || process.env.obfuscate;
   projectConfig.packageDir = 'node_modules';
   projectConfig.packageJson = 'package.json';
+  projectConfig.cardEntryObj = {};
 }
 
 function loadEntryObj(projectConfig) {
@@ -425,6 +426,7 @@ function readCardForm(form) {
     const cardPath = path.resolve(projectConfig.projectPath, '..', sourcePath + '.ets');
     if (cardPath && fs.existsSync(cardPath)) {
       projectConfig.entryObj['../' + sourcePath] = cardPath + '?entry';
+      projectConfig.cardEntryObj['../' + sourcePath] = cardPath;
       projectConfig.cardObj[cardPath] = sourcePath.replace(/^\.\//, '');
     }
   }
