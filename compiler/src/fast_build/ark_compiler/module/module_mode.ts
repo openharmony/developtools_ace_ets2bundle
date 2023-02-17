8a6f2eb1374c466c681aaef2d24f79fff4e67590
zhangrengao <zhangrengao1@huawei.com>
2023-02-17 17:08:11 +0800

    fixed 3d509ef from https://gitee.com/zhangrengao1/developtools_ace-ets2bundle/pulls/1892 Fix arkCompiler cluster exit listener always exist of rollup & webpack
    Signed-off-by: zhangrengao <zhangrengao1@huawei.com>
    Change-Id: I41668433ae28955337a297a72077682b1fa43978

--- compiler/src/fast_build/ark_compiler/module/module_mode.ts
+++ compiler/src/fast_build/ark_compiler/module/module_mode.ts
@@ -360,8 +360,8 @@ export class ModuleMode extends CommonMode {
 
   getSplittedModulesByNumber() {
     const result: any = [];
-    if (this.moduleInfos.size < this.workerNumber) {
-      for (const value of this.moduleInfos.values()) {
+    if (this.filterModuleInfos.size < this.workerNumber) {
+      for (const value of this.filterModuleInfos.values()) {
         result.push([value]);
       }
       return result;
@@ -372,7 +372,7 @@ export class ModuleMode extends CommonMode {
     }
 
     let pos: number = 0;
-    for (const value of this.moduleInfos.values()) {
+    for (const value of this.filterModuleInfos.values()) {
       const chunk = pos % this.workerNumber;
       result[chunk].push(value);
       pos++;
