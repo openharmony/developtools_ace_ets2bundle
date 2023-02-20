ef498033254cdda214db0755de89559ec6149fb7
bojiang <jiangbo91@huawei.com>
2023-02-20 16:08:09 +0800

    fixed fb46d39 from https://gitee.com/BoJiang4702/developtools_ace-ets2bundle/pulls/1899 jiangbo91@huawei.com
    Signed-off-by: bojiang <jiangbo91@huawei.com>
    Change-Id: I2563760a6bcea0fff60f50a694130b1e959d7bf1

---
+++ compiler/test/utForPartialUpdate/import/import@CustomDialog.ts
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+exports.source = `
+import { CustomDialogExample1 as CustomDialogExample } from './test/pages/import@CustomDialog'
+
+@Entry
+@Component
+struct CustomDialogUser {
+  @State textValue: string = ''
+  @State inputValue: string = 'click me'
+  dialogController: CustomDialogController = new CustomDialogController({
+    builder: CustomDialogExample({
+      cancel: this.onCancel,
+      confirm: this.onAccept,
+      textValue: $textValue,
+      inputValue: $inputValue
+    }),
+    cancel: this.existApp,
+    autoCancel: true,
+    alignment: DialogAlignment.Default,
+    offset: { dx: 0, dy: -20 },
+    gridCount: 4,
+    customStyle: false
+  })
+
+  onCancel() {
+    console.info('Callback when the first button is clicked')
+  }
+
+  onAccept() {
+    console.info('Callback when the second button is clicked')
+  }
+
+  existApp() {
+    console.info('Click the callback in the blank area')
+  }
+
+  build() {
+    Column() {
+      Button(this.inputValue)
+        .onClick(() => {
+          this.dialogController.open()
+        }).backgroundColor(0x317aff)
+    }.width('100%').margin({ top: 5 })
+  }
+}
+`
+
+exports.expectResult =
+`"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const import_CustomDialog_1 = require("./test/pages/import@CustomDialog");
+class CustomDialogUser extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1) {
+        super(parent, __localStorage, elmtId);
+        this.__textValue = new ObservedPropertySimplePU('', this, "textValue");
+        this.__inputValue = new ObservedPropertySimplePU('click me', this, "inputValue");
+        this.dialogController = new CustomDialogController({
+            builder: () => {
+                let jsDialog = new import_CustomDialog_1.CustomDialogExample1(this, {
+                    cancel: this.onCancel,
+                    confirm: this.onAccept,
+                    textValue: this.__textValue,
+                    inputValue: this.__inputValue
+                });
+                jsDialog.setController(this.dialogController);
+                ViewPU.create(jsDialog);
+            },
+            cancel: this.existApp,
+            autoCancel: true,
+            alignment: DialogAlignment.Default,
+            offset: { dx: 0, dy: -20 },
+            gridCount: 4,
+            customStyle: false
+        }, this);
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+        if (params.textValue !== undefined) {
+            this.textValue = params.textValue;
+        }
+        if (params.inputValue !== undefined) {
+            this.inputValue = params.inputValue;
+        }
+        if (params.dialogController !== undefined) {
+            this.dialogController = params.dialogController;
+        }
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+        this.__textValue.purgeDependencyOnElmtId(rmElmtId);
+        this.__inputValue.purgeDependencyOnElmtId(rmElmtId);
+    }
+    aboutToBeDeleted() {
+        this.__textValue.aboutToBeDeleted();
+        this.__inputValue.aboutToBeDeleted();
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    get textValue() {
+        return this.__textValue.get();
+    }
+    set textValue(newValue) {
+        this.__textValue.set(newValue);
+    }
+    get inputValue() {
+        return this.__inputValue.get();
+    }
+    set inputValue(newValue) {
+        this.__inputValue.set(newValue);
+    }
+    onCancel() {
+        console.info('Callback when the first button is clicked');
+    }
+    onAccept() {
+        console.info('Callback when the second button is clicked');
+    }
+    existApp() {
+        console.info('Click the callback in the blank area');
+    }
+    initialRender() {
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Column.create();
+            Column.width('100%');
+            Column.margin({ top: 5 });
+            if (!isInitialRender) {
+                Column.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Button.createWithLabel(this.inputValue);
+            Button.onClick(() => {
+                this.dialogController.open();
+            });
+            Button.backgroundColor(0x317aff);
+            if (!isInitialRender) {
+                Button.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        Button.pop();
+        Column.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
+loadDocument(new CustomDialogUser(undefined, {}));
+ViewStackProcessor.StopGetAccessRecording();
+`
