bcd72a4c0a66b1618caf87b6e8651c3c72d43088
libaohui <libaohui6@huawei.com>
2023-03-31 07:50:05 +0000

    Issue: #I6RZFT: rollup编译在非预览器模式下生成module_collection.json Description: rollup编译在非预览器模式下生成module_collection.json Future or Bugfix: Feature Binary Source: No
    Signed-off-by: libaohui <libaohui6@huawei.com>
    Change-Id: I38b8869b64cb3df0eeda943dd8ff9c964a1ef09a

--- compiler/src/fast_build/system_api/rollup-plugin-system-api.ts
+++ compiler/src/fast_build/system_api/rollup-plugin-system-api.ts
@@ -15,6 +15,7 @@
 
 import MagicString from 'magic-string';
 import { createFilter } from '@rollup/pluginutils';
+import path from 'path';
 
 import {
   NATIVE_MODULE,
@@ -25,14 +26,25 @@ import {
   systemModules,
   projectConfig
 } from '../../../main';
-import { writeUseOSFiles } from '../../utils';
+
+import { 
+  writeUseOSFiles,
+  writeCollectionFile,
+  getAllComponentsOrModules
+} from '../../utils';
 
 const filter: any = createFilter(/(?<!\.d)\.(ets|ts|js)$/);
+const allFiles: Set<string> = new Set();
+
+export const appImportModuleCollection: Map<string, Set<string>> = new Map();
 
 export function apiTransform() {
   const useOSFiles: Set<string> = new Set();
   return {
     name: 'apiTransform',
+    load(id: string) {
+      allFiles.add(path.join(id));
+    },
     transform(code: string, id: string) {
       const magicString = new MagicString(code);
       if (filter(id)) {
@@ -48,23 +60,33 @@ export function apiTransform() {
         map: magicString.generateMap({ hires: true })
       };
     },
+    beforeBuildEnd() {
+      this.share.allComponents = getAllComponentsOrModules(allFiles, 'component_collection.json');
+    },
     buildEnd() {
       if (projectConfig.isPreview && projectConfig.aceSoPath &&
         useOSFiles && useOSFiles.size > 0) {
         writeUseOSFiles(useOSFiles);
       }
+      if (!projectConfig.isPreview && !projectConfig.xtsMode) {
+        const allModules: Map<string, Array<string>> = getAllComponentsOrModules(allFiles, 'module_collection.json');
+        writeCollectionFile(projectConfig.cachePath, appImportModuleCollection, allModules, 'module_collection.json');
+      }
     }
   };
 }
 
+
 function processSystemApi(content: string, sourcePath: string): string {
   const REG_SYSTEM: RegExp =
     /import\s+(.+)\s+from\s+['"]@(system|ohos)\.(\S+)['"]|import\s+(.+)\s*=\s*require\(\s*['"]@(system|ohos)\.(\S+)['"]\s*\)/g;
+  appImportModuleCollection.set(path.join(sourcePath), new Set());
   return content.replace(REG_SYSTEM, (item, item1, item2, item3, item4, item5, item6) => {
     const moduleType: string = item2 || item5;
     const systemKey: string = item3 || item6;
     const systemValue: string = item1 || item4;
     const systemModule: string = `${moduleType}.${systemKey}`;
+    appImportModuleCollection.get(path.join(sourcePath)).add(systemModule);
     checkModuleExist(systemModule, sourcePath);
     if (NATIVE_MODULE.has(systemModule)) {
       item = `var ${systemValue} = globalThis.requireNativeModule('${moduleType}.${systemKey}')`;
@@ -100,16 +122,26 @@ function processLibso(content: string, sourcePath: string, useOSFiles: Set<strin
 // It is rare to use `import xxx = require('module')` for system module and user native library,
 // Here keep tackling with this for compatibility concern.
 function processSystemApiAndLibso(content: string, sourcePath: string, useOSFiles: Set<string>): string {
-  const REG_SYSTEM: RegExp = /import\s+(.+)\s*=\s*require\(\s*['"]@(system|ohos)\.(\S+)['"]\s*\)/g;
+  const REG_REQUIRE_SYSTEM: RegExp = /import\s+(.+)\s*=\s*require\(\s*['"]@(system|ohos)\.(\S+)['"]\s*\)/g;
   // Import libso should be recored in useOSFiles.
   const REG_LIB_SO: RegExp =
     /import\s+(.+)\s+from\s+['"]lib(\S+)\.so['"]|import\s+(.+)\s*=\s*require\(\s*['"]lib(\S+)\.so['"]\s*\)/g;
-
-  return content.replace(REG_SYSTEM, (_, item1, item2, item3, item4, item5, item6) => {
+  const REG_IMPORT_SYSTEM = /import\s+(.+)\s+from\s+['"]@(system|ohos)\.(\S+)['"]/g;
+  appImportModuleCollection.set(path.join(sourcePath), new Set());
+  content.replace(REG_IMPORT_SYSTEM, (_, item1, item2, item3, item4, item5, item6) => {
     const moduleType: string = item2 || item5;
     const systemKey: string = item3 || item6;
     const systemValue: string = item1 || item4;
     const systemModule: string = `${moduleType}.${systemKey}`;
+    appImportModuleCollection.get(path.join(sourcePath)).add(systemModule);
+    return _;
+  });
+  return content.replace(REG_REQUIRE_SYSTEM, (_, item1, item2, item3, item4, item5, item6) => {
+    const moduleType: string = item2 || item5;
+    const systemKey: string = item3 || item6;
+    const systemValue: string = item1 || item4;
+    const systemModule: string = `${moduleType}.${systemKey}`;
+    appImportModuleCollection.get(path.join(sourcePath)).add(systemModule);
     checkModuleExist(systemModule, sourcePath);
     return `import ${systemValue} from '@${moduleType}.${systemKey}'`;
   }).replace(REG_LIB_SO, (_, item1, item2, item3, item4) => {
