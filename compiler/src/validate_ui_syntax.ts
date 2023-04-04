7feaa64a88da04a7bd89711ef62111853a5cc0f3
hufeng <hufeng20@huawei.com>
2023-04-04 14:09:57 +0800

    Revert "Revert validateUiSyntax optimization"
    This reverts commit 7348d1aa02501d7d8c9a435555823c292e474906.
    
    Signed-off-by: hufeng <hufeng20@huawei.com>
    Change-Id: Iaa6a7e63aabdc96a8d91c66247a66bcacfe5950f

--- compiler/src/validate_ui_syntax.ts
+++ compiler/src/validate_ui_syntax.ts
@@ -152,17 +152,17 @@ export const sourcemapNamesCollection: Map<string, Map<string, string>> = new Ma
 export const originalImportNamesMap: Map<string, string> = new Map();
 
 export function validateUISyntax(source: string, content: string, filePath: string,
-  fileQuery: string): LogInfo[] {
+  fileQuery: string, newSourceFile: ts.SourceFile = null): LogInfo[] {
   let log: LogInfo[] = [];
   if (process.env.compileMode === 'moduleJson' ||
     path.resolve(filePath) !== path.resolve(projectConfig.projectPath || '', 'app.ets')) {
-    const res: LogInfo[] = checkComponentDecorator(source, filePath, fileQuery);
+    const res: LogInfo[] = checkComponentDecorator(source, filePath, fileQuery, newSourceFile);
     if (res) {
       log = log.concat(res);
     }
     const allComponentNames: Set<string> =
       new Set([...INNER_COMPONENT_NAMES, ...componentCollection.customComponents]);
-    checkUISyntax(filePath, allComponentNames, content, log);
+    checkUISyntax(filePath, allComponentNames, content, log, newSourceFile);
     componentCollection.customComponents.forEach(item => componentInfo.componentNames.add(item));
   }
 
@@ -170,9 +170,9 @@ export function validateUISyntax(source: string, content: string, filePath: stri
 }
 
 function checkComponentDecorator(source: string, filePath: string,
-  fileQuery: string): LogInfo[] | null {
+  fileQuery: string, newSourceFile: ts.SourceFile = null): LogInfo[] | null {
   const log: LogInfo[] = [];
-  const sourceFile: ts.SourceFile = ts.createSourceFile(filePath, source,
+  const sourceFile: ts.SourceFile = newSourceFile ? newSourceFile : ts.createSourceFile(filePath, source,
     ts.ScriptTarget.Latest, true, ts.ScriptKind.ETS);
   if (sourceFile && sourceFile.statements && sourceFile.statements.length) {
     const result: DecoratorResult = {
@@ -357,9 +357,9 @@ function collectLocalStorageName(node: ts.Decorator): void {
 }
 
 function checkUISyntax(filePath: string, allComponentNames: Set<string>, content: string,
-  log: LogInfo[]): void {
-  const sourceFile: ts.SourceFile = ts.createSourceFile(filePath, content,
-    ts.ScriptTarget.Latest, true, ts.ScriptKind.ETS);
+  log: LogInfo[], newSourceFile: ts.SourceFile = null): void {
+  const sourceFile: ts.SourceFile = newSourceFile ? newSourceFile :
+    ts.createSourceFile(filePath, content, ts.ScriptTarget.Latest, true, ts.ScriptKind.ETS);
   visitAllNode(sourceFile, sourceFile, allComponentNames, log);
 }
 
