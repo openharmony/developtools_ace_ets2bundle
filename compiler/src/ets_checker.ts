ff0cc6f1072e711f13f768377e484cbf26437d87
hufeng <hufeng20@huawei.com>
2023-04-23 09:43:20 +0800

    Remove ts.createSourceFile invoking in processImport in compilerMode
    Signed-off-by: hufeng <hufeng20@huawei.com>
    Change-Id: I80b748bb4761de58cc1751b37bb6fec02c1b6543

--- compiler/src/ets_checker.ts
+++ compiler/src/ets_checker.ts
@@ -58,6 +58,14 @@ import { isExtendFunction, isOriginalExtend } from './process_ui_syntax';
 import { visualTransform } from './process_visual';
 import { tsWatchEmitter } from './fast_build/ets_ui/rollup-plugin-ets-checker';
 
+export const SOURCE_FILES: Map<string, ts.SourceFile> = new Map();
+
+function collectSourceFilesMap(program: ts.Program) {
+  program.getSourceFiles().forEach(sourceFile => {
+    SOURCE_FILES.set(path.normalize(sourceFile.fileName), sourceFile);
+  });
+}
+
 export function readDeaclareFiles(): string[] {
   const declarationsFileNames: string[] = [];
   fs.readdirSync(path.resolve(__dirname, '../declarations'))
@@ -215,6 +223,7 @@ export function serviceChecker(rootFileNames: string[], newLogger: any = null): 
     languageService = createLanguageService(filterFiles);
   }
   globalProgram.program = languageService.getProgram();
+  collectSourceFilesMap(globalProgram.program);
   const allDiagnostics: ts.Diagnostic[] = globalProgram.program
     .getSyntacticDiagnostics()
     .concat(globalProgram.program.getSemanticDiagnostics())
