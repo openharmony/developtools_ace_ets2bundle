6ed82de1a141fa5682576361c0f049854fc693f3
houhaoyu <houhaoyu@huawei.com>
2023-05-21 19:04:20 +0800

    houhaoyu@huawei.com
    d.ets not check initialization
    
    Signed-off-by: houhaoyu <houhaoyu@huawei.com>
    Change-Id: I866285113fc50b385bf5b54729de8715933622c1

--- compiler/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
+++ compiler/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
@@ -297,6 +297,7 @@ function clearCollection(): void {
   CUSTOM_BUILDER_METHOD.clear();
   GLOBAL_CUSTOM_BUILDER_METHOD.clear();
   INNER_CUSTOM_BUILDER_METHOD.clear();
+  storedFileInfo.getCurrentArkTsFile().compFromDETS.clear();
 }
 
 function resetCollection() {
