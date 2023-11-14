a17f9b6d952d6dd2528ff2c87a65f2d8d08d2df3
zhangjiayong1 <zhangjiayong3@huawei.com>
2023-11-14 03:52:55 +0000

    customNode增加extraInfo信息
    Signed-off-by: zhangjiayong1 <zhangjiayong3@huawei.com>

--- compiler/src/process_component_build.ts
+++ compiler/src/process_component_build.ts
@@ -1112,7 +1112,7 @@ function processTabAndNav(node: ts.ExpressionStatement, innerCompStatements: ts.
   }
 }
 
-function getRealNodePos(node: ts.Node): number {
+export function getRealNodePos(node: ts.Node): number {
   // @ts-ignore
   if (node.pos === -1 && node.expression) {
     // @ts-ignore
