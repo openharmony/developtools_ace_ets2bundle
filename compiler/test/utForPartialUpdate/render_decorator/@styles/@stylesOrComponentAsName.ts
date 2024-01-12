120ebe6b8fd366587a830598cc1964be7e6f6795
bojiang <jiangbo91@huawei.com>
2024-01-12 11:23:51 +0800

    jiangbo91@huawei.com
    Signed-off-by: bojiang <jiangbo91@huawei.com>
    Change-Id: I1251c38cc775d85683d04ce9c4f0fe1b75408a19

---
+++ compiler/test/utForPartialUpdate/render_decorator/@styles/@stylesOrComponentAsName.ts
@@ -0,0 +1,271 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+exports.source = `
+@Entry
+@Component
+struct Index {
+  @Styles Index() {
+    .backgroundColor(Color.Blue)
+  }
+  build() {
+    Row() {
+      Text('111')
+        .width(30)
+        .Index()
+        .height(30)
+      ComB();
+      ComA();
+      MyComponent();
+    }
+  }
+}
+
+@Component
+struct ComA {
+   @Styles ComA() {
+    .backgroundColor(Color.Blue)
+  }
+  build() {
+    Row() {
+      Text('111')
+        .ComA()
+        .width(30)
+        .height(30)
+    }
+  }
+}
+
+@Component
+struct ComB {
+   @Styles ComB() {
+    .backgroundColor(Color.Blue)
+  }
+  build() {
+    Row() {
+      Text('111')
+        .width(30)
+        .height(30)
+        .ComB()
+    }
+  }
+}
+
+@Component
+struct MyComponent {
+  @Styles MyComponent() {
+    .backgroundColor(Color.Gray)
+  }
+
+  build() {
+    Column() {
+      Text('Text1')
+        .fontSize(50)
+        .fontColor(Color.White)
+        .stateStyles({
+          normal: this.MyComponent,
+        })
+    }
+  }
+}
+`
+
+exports.expectResult =
+`"use strict";
+class Index extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Row.create();
+        }, Row);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create('111');
+            Text.width(30);
+            Text.backgroundColor(Color.Blue);
+            Text.height(30);
+        }, Text);
+        Text.pop();
+        {
+            this.observeComponentCreation2((elmtId, isInitialRender) => {
+                if (isInitialRender) {
+                    let paramsLambda = () => {
+                        return {};
+                    };
+                    ViewPU.create(new ComB(this, {}, undefined, elmtId, paramsLambda, { page: "@stylesOrComponentAsName.ets", line: 14 }));
+                }
+                else {
+                    this.updateStateVarsOfChildByElmtId(elmtId, {});
+                }
+            }, { name: "ComB" });
+        }
+        {
+            this.observeComponentCreation2((elmtId, isInitialRender) => {
+                if (isInitialRender) {
+                    let paramsLambda = () => {
+                        return {};
+                    };
+                    ViewPU.create(new ComA(this, {}, undefined, elmtId, paramsLambda, { page: "@stylesOrComponentAsName.ets", line: 15 }));
+                }
+                else {
+                    this.updateStateVarsOfChildByElmtId(elmtId, {});
+                }
+            }, { name: "ComA" });
+        }
+        {
+            this.observeComponentCreation2((elmtId, isInitialRender) => {
+                if (isInitialRender) {
+                    let paramsLambda = () => {
+                        return {};
+                    };
+                    ViewPU.create(new MyComponent(this, {}, undefined, elmtId, paramsLambda, { page: "@stylesOrComponentAsName.ets", line: 16 }));
+                }
+                else {
+                    this.updateStateVarsOfChildByElmtId(elmtId, {});
+                }
+            }, { name: "MyComponent" });
+        }
+        Row.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+class ComA extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Row.create();
+        }, Row);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create('111');
+            Text.backgroundColor(Color.Blue);
+            Text.width(30);
+            Text.height(30);
+        }, Text);
+        Text.pop();
+        Row.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+class ComB extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Row.create();
+        }, Row);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create('111');
+            Text.width(30);
+            Text.height(30);
+            Text.backgroundColor(Color.Blue);
+        }, Text);
+        Text.pop();
+        Row.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+class MyComponent extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Column.create();
+        }, Column);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create('Text1');
+            Text.fontSize(50);
+            Text.fontColor(Color.White);
+            ViewStackProcessor.visualState("normal");
+            Text.backgroundColor(Color.Gray);
+            ViewStackProcessor.visualState();
+        }, Text);
+        Text.pop();
+        Column.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
+loadDocument(new Index(undefined, {}));
+ViewStackProcessor.StopGetAccessRecording();
+`
