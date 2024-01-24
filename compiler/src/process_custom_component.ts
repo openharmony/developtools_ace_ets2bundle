49a861fd2e13786c7e9c11852d651e455ddc6b55
bojiang <jiangbo91@huawei.com>
2024-01-24 20:09:55 +0800

    jiangbo91@huawei.com
    @Link check warn upgrade to ERROR
    
    Signed-off-by: bojiang <jiangbo91@huawei.com>
    Change-Id: I4568971eec8931b7feb1fe4810b2ad670f243448

--- compiler/src/process_custom_component.ts
+++ compiler/src/process_custom_component.ts
@@ -889,7 +889,7 @@ function validateMandatoryToInitViaParam(node: ts.CallExpression, customComponen
   mandatoryToInitViaParamSet.forEach(item => {
     if (item && !curChildProps.has(item)) {
       log.push({
-        type: LogType.WARN,
+        type: LogType.ERROR,
         message: `Property '${item}' in the custom component '${customComponentName}'` +
           ` is missing (mandatory to specify).`,
         pos: node.getStart()
