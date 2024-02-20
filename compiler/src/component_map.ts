901c944234704f486a1e345986b12bde47b2d723
baosiyuan <baosiyuan@huawei.com>
2024-02-20 16:53:18 +0800

    Add config for MediaCachedImage
    Signed-off-by: baosiyuan <baosiyuan@huawei.com>

--- compiler/src/component_map.ts
+++ compiler/src/component_map.ts
@@ -109,7 +109,7 @@ export const JS_BIND_COMPONENTS: Set<string> = new Set([
   'XComponentController', 'CanvasRenderingContext2D', 'CanvasGradient', 'ImageBitmap', 'ImageData',
   'Path2D', 'RenderingContextSettings', 'OffscreenCanvasRenderingContext2D', 'DatePickerDialog',
   'TextPickerDialog', 'AlertDialog', 'ContextMenu', 'ActionSheet', 'PatternLockController',
-  'TimePickerDialog', 'CalendarPickerDialog'
+  'TimePickerDialog', 'CalendarPickerDialog', 'MediaCachedImage'
 ]);
 
 export const NEEDPOP_COMPONENT: Set<string> = new Set(['Blank', 'Search']);
