34384b6592d33dfd14095385473c3a902c4318ba
ctw-ian <chentingwei2@huawei.com>
2024-03-23 02:19:05 +0000

    Add ohmurl transformation for @arkts modules
    Issue:https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I9AX4U
    
    Signed-off-by: ctw-ian <chentingwei2@huawei.com>
    Change-Id: I5df5b9f2f76a71fc50e101feab904b3b270c861c

--- compiler/src/fast_build/system_api/api_check_utils.ts
+++ compiler/src/fast_build/system_api/api_check_utils.ts
@@ -33,6 +33,7 @@ import {
 } from '../../utils';
 import { type ResolveModuleInfo } from '../../ets_checker';
 import {
+  ARKTS_MODULE_PREFIX,
   GLOBAL_DECLARE_WHITE_LIST,
   FIND_MODULE_WARNING,
   FORM_TAG_CHECK_NAME,
@@ -173,7 +174,7 @@ export function getRealModulePath(apiDirs: string[], moduleName: string, exts: s
 export function moduleRequestCallback(moduleRequest: string, _: string,
   moduleType: string, systemKey: string): string {
   for (let config of extendSdkConfigs.values()) {
-    if (config.prefix === '@arkui-x') {
+    if (config.prefix === '@arkui-x' || config.prefix === ARKTS_MODULE_PREFIX) {
       continue;
     }
     if (moduleRequest.startsWith(config.prefix + '.')) {
