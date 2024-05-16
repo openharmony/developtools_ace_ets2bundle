07ea3a171d4b513caa946e4ea2729cd064139d51
lihong <lihong67@huawei.com>
2024-05-16 18:07:31 +0800

    lihong67@huawei.com
    remove validate two way bind for @ComponentV1.
    
    Signed-off-by: lihong <lihong67@huawei.com>
    Change-Id: Ibfefa002df2817418c19b2b91d90c9e340b45cea

--- compiler/src/process_custom_component.ts
+++ compiler/src/process_custom_component.ts
@@ -589,6 +589,11 @@ export function assignmentFunction(componeParamName: string): ts.ExpressionState
 
 function traverseChildComponentArgs(childParam: ts.Expression[], name: string, log: LogInfo[],
   componentNode: ts.CallExpression): ts.Expression[] {
+  const childStructInfo: StructInfo = processStructComponentV2.getAliasStructInfo(componentNode) ||
+    processStructComponentV2.getOrCreateStructInfo(name);
+  if (!childStructInfo.isComponentV2) {
+    return childParam;
+  }
   const objectLiteralIndex: number = 2;
   if (childParam.length > objectLiteralIndex && ts.isObjectLiteralExpression(childParam[1]) &&
     childParam[1].properties) {
@@ -596,7 +601,7 @@ function traverseChildComponentArgs(childParam: ts.Expression[], name: string, l
     childParam[1].properties.forEach((item: ts.PropertyAssignment) => {
       if (item.name && ts.isIdentifier(item.name)) {
         const itemName: string = item.name.escapedText.toString();
-        updatePropertyAssignment(newProperties, itemName, item, name, log, componentNode);
+        updatePropertyAssignment(newProperties, itemName, item, childStructInfo, log);
       }
     });
     if (newProperties.length) {
@@ -622,8 +627,7 @@ function getNewArgsForCustomComponent(childParam: ts.Expression[],
 }
 
 function updatePropertyAssignment(newProperties: ts.PropertyAssignment[],
-  itemName: string, item: ts.PropertyAssignment, childName: string,
-  log: LogInfo[], componentNode: ts.CallExpression): void {
+  itemName: string, item: ts.PropertyAssignment, childStructInfo: StructInfo, log: LogInfo[]): void {
   if (isDoubleNonNullExpression(item.initializer)) {
     if (isLeftHandExpression(item.initializer.expression.expression)) {
       const result: Record<string, boolean> = { hasQuestionToken: false };
@@ -636,9 +640,7 @@ function updatePropertyAssignment(newProperties: ts.PropertyAssignment[],
         });
         return;
       }
-      const childStructInfo: StructInfo = processStructComponentV2.getAliasStructInfo(componentNode) ||
-        processStructComponentV2.getOrCreateStructInfo(childName);
-      if (childStructInfo.isComponentV2 && childStructInfo.paramDecoratorMap.has(itemName) &&
+      if (childStructInfo.paramDecoratorMap.has(itemName) &&
         childStructInfo.eventDecoratorSet.has('$' + itemName)) {
         newProperties.push(createUpdateTwoWayNode(itemName, item.initializer.expression.expression));
         return;
