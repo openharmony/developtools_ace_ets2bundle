4daaf83de526f873d46aace64508e03a7d73173b
huangyu <huangyu76@huawei.com>
2024-06-22 22:03:05 +0800

    Reuse language service when version of dependency changes to optimize incremental performance
    Issue: https://gitee.com/openharmony/developtools_ace_ets2bundle/issues/IA7HXH
    Test: npm run test, npm run arkTest
    
    Signed-off-by: huangyu <huangyu76@huawei.com>
    Change-Id: Ib864e5b23688d5cb65800bc720ba170016680031

--- compiler/test/ark_compiler_ut/ets_checker.test.ts
+++ compiler/test/ark_compiler_ut/ets_checker.test.ts
@@ -18,7 +18,13 @@ import fs from 'fs';
 import { expect } from 'chai';
 
 import RollUpPluginMock from './mock/rollup_mock/rollup_plugin_mock';
-import { addLocalPackageSet, localPackageSet } from '../../lib/ets_checker';
+import {
+  addLocalPackageSet,
+  localPackageSet,
+  needReCheckForChangedDepUsers,
+  resetEtsCheck,
+  serviceChecker,
+} from '../../lib/ets_checker';
 import { projectConfig } from '../../main';
 
 mocha.describe('test ets_checker file api', function () {
@@ -62,4 +68,38 @@ mocha.describe('test ets_checker file api', function () {
         expect(localPackageSet.has('pkgname')).to.be.true;
     });
 
+    mocha.it('1-3: test getOrCreateLanguageService when dependency in oh-package.json change', function () {
+        this.timeout(10000);
+        this.rollup.build();
+        let rollupObject = this.rollup;
+        expect(needReCheckForChangedDepUsers).to.be.false;
+
+        interface MockCacheStore {
+            service: Object | undefined;
+            pkgJsonFileHash: string;
+            targetESVersion: number;
+        }
+        const mockServiceCache = {
+            service: undefined,
+            pkgJsonFileHash: '9f07917d395682c73a90af8f5796a2c6',
+            targetESVersion: 8
+        }
+        let mockCache = new Map<string, MockCacheStore>();
+        mockCache.set('service', mockServiceCache);
+        rollupObject.share.cache = mockCache;
+        rollupObject.share.depInfo = {enableIncre: true};
+        rollupObject.share.projectConfig.pkgJsonFileHash = '26bde0f30dda53b0afcbf39428ec9851';
+        Object.assign(projectConfig, rollupObject.share.projectConfig);
+
+        // The current hash and the hash in cache of the dependency differs, should recheck
+        serviceChecker(['../testdata/expect/expect_Index.ets'], null, null, null, rollupObject.share);
+        expect(needReCheckForChangedDepUsers).to.be.true;
+
+        resetEtsCheck();
+        expect(needReCheckForChangedDepUsers).to.be.false;
+
+        // The current hash and the hash in cache of the dependency are the same, no need to recheck
+        serviceChecker(['../testdata/expect/expect_Index.ets'], null, null, null, rollupObject.share);
+        expect(needReCheckForChangedDepUsers).to.be.false;
+    })
 });
