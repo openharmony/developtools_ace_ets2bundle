25b0bf05311dcc75ab3640a1b128f25093c33190
BoJiang <jiangbo91@huawei.com>
2024-07-18 17:10:43 +0800

    jiangbo91@huawei.com
    Signed-off-by: BoJiang <jiangbo91@huawei.com>
    Change-Id: Ia67bf7d6aa7f2dddd71939c777a06c33e623e77c

--- compiler/test/utForPartialUpdate/render_decorator/@builder/@builderSimplifyAfferent.ts
+++ compiler/test/utForPartialUpdate/render_decorator/@builder/@builderSimplifyAfferent.ts
@@ -46,6 +46,9 @@ exports.expectResult =
 if (!("finalizeConstruction" in ViewPU.prototype)) {
     Reflect.set(ViewPU.prototype, "finalizeConstruction", () => { });
 }
+if (PUV2ViewBase.contextStack === undefined) {
+    Reflect.set(PUV2ViewBase, "contextStack", []);
+}
 const test_value = "hello";
 class testDemo extends ViewPU {
     constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
@@ -67,6 +70,7 @@ class testDemo extends ViewPU {
         this.aboutToBeDeletedInternal();
     }
     initialRender() {
+        PUV2ViewBase.contextStack && PUV2ViewBase.contextStack.push(this);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Column.create();
         }, Column);
@@ -76,11 +80,14 @@ class testDemo extends ViewPU {
             Text.fontWeight(FontWeight.Bold);
         }, Text);
         Text.pop();
-        overBuilder.bind(this)(makeBuilderParameterProxy("overBuilder", { test_value: () => test_value }), this);
+        overBuilder.bind(this)(makeBuilderParameterProxy("overBuilder", { test_value: () => test_value }));
         Column.pop();
+        PUV2ViewBase.contextStack && PUV2ViewBase.contextStack.pop();
     }
     rerender() {
+        PUV2ViewBase.contextStack && PUV2ViewBase.contextStack.push(this);
         this.updateDirtyElements();
+        PUV2ViewBase.contextStack && PUV2ViewBase.contextStack.pop();
     }
 }
 function overBuilder($$, parent = null) {
