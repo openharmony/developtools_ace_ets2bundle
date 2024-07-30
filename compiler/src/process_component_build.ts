05e9d13735b5ee979cd03b0c1d7473aff313f4d8
lixinnan <lixinnan1@huawei.com>
2024-07-30 15:17:21 +0800

    Add reviewers V1
    Signed-off-by: lixinnan <lixinnan1@huawei.com>
    Change-Id: Ib3229c6edf16c0c94378e1cb6078079712d503c1

--- compiler/src/process_component_build.ts
+++ compiler/src/process_component_build.ts
@@ -730,7 +730,6 @@ function processExpressionStatementChange(node: ts.ExpressionStatement, nextNode
   }
 }
 
-
 function processBlockToExpression(node: ts.ExpressionStatement, nextNode: ts.Block,
   log: LogInfo[], name: string, isPropertyAccessExpressionNode: boolean): ts.ExpressionStatement {
   const childParam: string = isPropertyAccessExpressionNode ? [...storedFileInfo.overallBuilderParamCollection.get(name)].slice(-1)[0] :
