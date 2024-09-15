000c3ffdeb0f6a2ed75eb8c644b8eb9e3f99eb09
ElevenDuan <duanshiyi1@huawei.com>
2024-09-15 17:19:53 +0800

    Accurate update version
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IATGCV
    Signed-off-by: ElevenDuan <duanshiyi1@huawei.com>
    Change-Id: Ife3a21b81f7a0d6199d92f7d73a40eb718893984

--- compiler/main.js
+++ compiler/main.js
@@ -123,6 +123,8 @@ function initProjectConfig(projectConfig) {
   projectConfig.optTryCatchFunc = true;
   // All files which dependent on bytecode har, and should be added to compilation entries.
   projectConfig.otherCompileFiles = {};
+  // Packages which need to update version in bytecode har 
+  projectConfig.updateVersionInfo = {};
 }
 
 function loadEntryObj(projectConfig) {
@@ -599,6 +601,9 @@ function loadBuildJson() {
   }
   if (!!aceBuildJson.byteCodeHar) { 
     projectConfig.useTsHar = true;
+  }
+  if (aceBuildJson.updateVersionInfo) {
+    projectConfig.updateVersionInfo = aceBuildJson.updateVersionInfo;
   }
 }
 
@@ -1099,6 +1104,7 @@ function resetProjectConfig() {
     }
   }
   projectConfig.otherCompileFiles = {};
+  projectConfig.updateVersionInfo = {};
 }
 
 function resetGlobalProgram() {
