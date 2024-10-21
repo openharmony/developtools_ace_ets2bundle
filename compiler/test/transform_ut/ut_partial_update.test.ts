43f789f9dd71c1b478c521695b491a508f2eb59e
s00912778 <shijiakai2@huawei.com>
2024-10-21 14:34:08 +0800

    shijiakai2@huawei.com
    restore pathConfig
    
    Signed-off-by: s00912778 <shijiakai2@huawei.com>
    Change-Id: I31de60b8b8f0621ce37e5fe72b3d581760dd5e25

--- compiler/test/transform_ut/ut_partial_update.test.ts
+++ compiler/test/transform_ut/ut_partial_update.test.ts
@@ -40,7 +40,10 @@ import {
 	CACHE_PATH,
 	UT_PARTIAL_UPFATE_PAGES
 } from './helpers/pathConfig';
-import { parseCode, sourceReplace } from './helpers/parser';
+import { 
+	parseCode, 
+	sourceReplace 
+} from './helpers/parser';
 
 const PROJECT_ROOT: string = path.resolve(__dirname, '../../test/transform_ut');
 const DEFAULT_PROJECT: string = 'application';
