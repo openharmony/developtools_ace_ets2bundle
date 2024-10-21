6ddfaaa9ec563f5d8cb881de0cc8fe631da7ff86
s00912778 <shijiakai2@huawei.com>
2024-10-21 16:11:44 +0800

    shijiakai2@huawei.com
    add ut for partial update
    
    Signed-off-by: s00912778 <shijiakai2@huawei.com>
    Change-Id: Iada64210fae5af9e3ebee0990cdc99eeb3fb8ea2

--- compiler/test/transform_ut/application/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/utForPartialUpdate/inner_component_transform/render_component/tab/tab.js
+++ compiler/test/transform_ut/application/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/utForPartialUpdate/inner_component_transform/render_component/tab/tab.js
@@ -1,58 +1,4 @@
-/*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-exports.source = `
-@Entry
-@Component
-struct TabSimple {
-  private controller: TabsController = new TabsController();
-  build() {
-    Column() {
-      Tabs({ barPosition: BarPosition.Start, index: 1, controller: this.controller}) {
-        TabContent() {
-          Flex() {
-            Column() {
-              Text('text1')
-              .height(100)
-              .width(200)
-              Text('xxx')
-              .height(100)
-              .width(200)
-            }
-            .height(100)
-            .width(200)
-          }
-            .height(100)
-            .width(200)
-        }
-        .tabBar("TabBar")
-        .height(100)
-        .width(200)
-        TabContent() {
-          Text('text2')
-        }
-        .tabBar("TabBar 2")
-        .height(100)
-        .width(200)
-      }
-    }
-  }
-}
-`
-exports.expectResult =
-`"use strict";
+"use strict";
 if (!("finalizeConstruction" in ViewPU.prototype)) {
     Reflect.set(ViewPU.prototype, "finalizeConstruction", () => { });
 }
@@ -136,8 +82,9 @@ class TabSimple extends ViewPU {
     rerender() {
         this.updateDirtyElements();
     }
+    static getEntryName() {
+        return "TabSimple";
+    }
 }
-ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
-loadDocument(new TabSimple(undefined, {}));
-ViewStackProcessor.StopGetAccessRecording();
-`
+registerNamedRoute(() => new TabSimple(undefined, {}), "", { bundleName: "com.example.application", moduleName: "application", pagePath: "pages/utForPartialUpdate/inner_component_transform/render_component/tab/tab", pageFullPath: "application/entry/src/main/ets/pages/utForPartialUpdate/inner_component_transform/render_component/tab/tab", integratedHsp: "false" });
+//# sourceMappingURL=tab.js.map
