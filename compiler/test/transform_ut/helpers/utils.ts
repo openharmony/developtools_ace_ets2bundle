e93e60cec7b23b636dd03e7d5d5fe6dc5dc9ed58
s00912778 <shijiakai2@huawei.com>
2024-10-23 16:15:12 +0800

    shijiakai2@huawei.com
    fix @Type ut has no error
    
    Signed-off-by: s00912778 <shijiakai2@huawei.com>
    Change-Id: Id8e57b1f7c7f8777eb01db5de0d7a4b7cb8c723c

--- compiler/test/transform_ut/helpers/utils.ts
+++ compiler/test/transform_ut/helpers/utils.ts
@@ -19,3 +19,22 @@ export function scanFiles(filepath: string, fileList: Set<string>) {
 		}
 	});
 }
+
+export function scanFileNames(filepath: string, fileList: Set<string>) {
+	if (!fs.existsSync(filepath)) {
+		return;
+	}
+	const files = fs.readdirSync(filepath);
+	files.forEach((file) => {
+		const child = path.join(filepath, file);
+		const stat = fs.statSync(child);
+		if (stat.isDirectory()) {
+			scanFiles(child, fileList);
+		} else {
+			if (child.includes("mock")) {
+				return;
+			}
+			fileList.add(path.basename(child));
+		}
+	});
+}
