c727d8874161848b8d08a0649d8272eee7dfa4c8
xuhangqi <xuhangqi@huawei.com>
2024-10-23 16:31:58 +0800

    Fix bytecode har hot reload bug
    Issue:
    https: //gitee.com/openharmony/developtools_ace_ets2bundle/issues/IAZ88S
    Signed-off-by: xuhangqi <xuhangqi@huawei.com>
    Change-Id: Ie53dd58387c5cfd907df9fbbfddc277f7cd2aef5

--- compiler/src/fast_build/ark_compiler/module/module_hotreload_mode.ts
+++ compiler/src/fast_build/ark_compiler/module/module_hotreload_mode.ts
@@ -83,7 +83,7 @@ export class ModuleHotreloadMode extends ModuleMode {
   private compileAllFiles(rollupObject: Object, parentEvent: Object): void {
     this.prepareForCompilation(rollupObject, parentEvent);
     SourceMapGenerator.getInstance().buildModuleSourceMapInfo(parentEvent);
-    this.generateAbcByEs2abc(parentEvent);
+    this.generateAbcByEs2abc(parentEvent, !!this.projectConfig.byteCodeHarInfo);
   }
 
   private compileChangeListFiles(rollupObject: Object, parentEvent: Object): void {
@@ -130,7 +130,8 @@ export class ModuleHotreloadMode extends ModuleMode {
     const outputABCPath: string = path.join(this.projectConfig.patchAbcPath, MODULES_ABC);
     validateFilePathLength(outputABCPath, this.logger);
     this.moduleAbcPath = outputABCPath;
-    this.generateAbcByEs2abc(parentEvent);
+    // During incremental compilation, the bytecode har path must be blocked from being written to filesInfo.
+    this.generateAbcByEs2abc(parentEvent, false);
   }
 
   private parseChangedFileListJson(changedFileListJson: string): object {
@@ -181,9 +182,10 @@ export class ModuleHotreloadMode extends ModuleMode {
     stopEvent(eventUpdateSourceMapFromFileList);
   }
 
-  private generateAbcByEs2abc(parentEvent: Object): void {
+  private generateAbcByEs2abc(parentEvent: Object, includeByteCodeHarInfo: boolean): void {
     this.generateEs2AbcCmd();
     this.addHotReloadArgs();
+    this.genDescriptionsForMergedEs2abc(includeByteCodeHarInfo);
     this.generateMergedAbcOfEs2Abc(parentEvent);
   }
 }
