a5504f1dfe3b65053911cdfe4a5d87b52cabbbc3
张全超 <zhangquanchao@huawei.com>
2024-12-19 18:52:42 +0800

    提供单独导航点组件
    Signed-off-by: 张全超 <zhangquanchao@huawei.com>

--- compiler/build_declarations_file.js
+++ compiler/build_declarations_file.js
@@ -23,7 +23,7 @@ const addTSInterfaceSet = ['ForEach', 'LazyForEach', 'TapGesture', 'LongPressGes
 const addTSAttributeSet = ['AlphabetIndexer', 'Animator', 'Badge', 'Blank', 'Button', 'Calendar', 'CalendarPicker',
   'Canvas', 'Checkbox', 'CheckboxGroup', 'Circle', 'Column', 'ColumnSplit', 'Counter', 'DataPanel', 'DatePicker',
   'Divider', 'Ellipse', 'Flex', 'FormComponent', 'Gauge', 'Grid', 'GridItem', 'GridContainer', 'Image',
-  'ImageAnimator', 'Line', 'LinearIndicator', 'List', 'ListItem', 'ListItemGroup', 'LoadingProgress', 'Marquee', 'Navigation',
+  'ImageAnimator', 'IndicatorComponent', 'Line', 'LinearIndicator', 'List', 'ListItem', 'ListItemGroup', 'LoadingProgress', 'Marquee', 'Navigation',
   'Navigator', 'Panel', 'Path', 'PatternLock', 'Piece', 'PluginComponent', 'Polygon', 'Polyline', 'Progress',
   'QRCode', 'Radio', 'Rating', 'Rect', 'Refresh', 'Row', 'RowSplit', 'Scroll', 'ScrollBar', 'Search',
   'Select', 'Shape', 'Sheet', 'Slider', 'Span', 'Stack', 'Stepper', 'StepperItem', 'Swiper',
