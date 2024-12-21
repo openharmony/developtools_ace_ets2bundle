9fa3c54b91bdea91cf9ab774aa804db8690e1d67
lixinnan <lixinnan1@huawei.com>
2024-12-21 14:38:05 +0800

    增加Span组件动态添加属性UT用例
    Signed-off-by: lixinnan <lixinnan1@huawei.com>
    Change-Id: I855c14bfa3eb4e5a4d413c60c9df28e73933bae2

---
+++ compiler/test/utForPartialUpdate/render_decorator/@extend/@extendSpanComponent.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+* When the @Extend decorator is used to dynamically add common attributes to the Span component, a warning log is generated.
+* Cause: The Span component does not support the use of common attributes.
+*/
+exports.source = `
+@Extend(Span)
+function spanStyle() {
+  .width(50)
+}
+
+@Entry
+@Component
+struct SpanTest {
+  build() {
+    Column() {
+      Text('Test Page') {
+        Span('Span Test')
+          .spanStyle()
+      }
+    }
+  }
+}
+`;
+
+exports.expectResult =
+`"use strict";
+if (!("finalizeConstruction" in ViewPU.prototype)) {
+    Reflect.set(ViewPU.prototype, "finalizeConstruction", () => { });
+}
+function __Span__spanStyle() {
+    Span.width(50);
+}
+class SpanTest extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.setInitiallyProvidedValue(params);
+        this.finalizeConstruction();
+    }
+    setInitiallyProvidedValue(params) {
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Column.create();
+        }, Column);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create('Test Page');
+        }, Text);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Span.create('Span Test');
+            __Span__spanStyle();
+        }, Span);
+        Text.pop();
+        Column.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
+loadDocument(new SpanTest(undefined, {}));
+ViewStackProcessor.StopGetAccessRecording();
+`;
