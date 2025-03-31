de60ff2ba94e6d2420aa2846a36b525128ccdb29
lixingchi1 <lixingchi1@huawei.com>
2025-03-31 15:47:33 +0800

    support interop compile debug 0331 1547
    Signed-off-by: lixingchi1 <lixingchi1@huawei.com>

--- arkui-plugins/test/demo/hello_world/declgenBridgeCodePath/entry/new.ts
+++
@@ -1,7 +0,0 @@
-let ETSGLOBAL = (globalThis as any).Panda.getClass('Lentry/new/ETSGLOBAL;');
-ETSGLOBAL._$init$_();
-export { MyStateSample, Child } from "./index";
-
-export const MyStateSample1 = (globalThis as any).Panda.getClass('Lentry/new/MyStateSample1;');
-
-export const Child1 = (globalThis as any).Panda.getClass('Lentry/new/Child1;');
