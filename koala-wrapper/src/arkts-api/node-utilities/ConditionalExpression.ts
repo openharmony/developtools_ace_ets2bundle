c6e8758e2fece9322d634b7b736e604bda6a17a7
Jiakai Shi <shijiakai2@huawei.com>
2025-04-11 20:34:09 +0800

    add node utilities to skip unnecessary update
    Signed-off-by: Jiakai Shi <shijiakai2@huawei.com>
    Change-Id: Ibb8219d0c5e438d61e2980abec2510cd710a7819

---
+++ koala-wrapper/src/arkts-api/node-utilities/ConditionalExpression.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ConditionalExpression, Expression } from '../../generated';
+import { isSameNativeObject } from '../peers/ArktsObject';
+import { attachModifiers, updateThenAttach } from '../utilities/private';
+
+export function updateConditionalExpression(
+    original: ConditionalExpression,
+    test?: Expression,
+    consequent?: Expression,
+    alternate?: Expression
+): ConditionalExpression {
+    if (
+        isSameNativeObject(test, original.test) &&
+        isSameNativeObject(consequent, original.consequent) &&
+        isSameNativeObject(alternate, original.alternate)
+    ) {
+        return original;
+    }
+
+    const update = updateThenAttach(ConditionalExpression.updateConditionalExpression, attachModifiers);
+    return update(original, test, consequent, alternate);
+}
