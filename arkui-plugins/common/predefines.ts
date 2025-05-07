70f212c567cf940e93e069e68edc87924074ae78
l00913061 <lishihao16@huawei.com>
2025-05-07 17:41:31 +0800

    customdialog
    Signed-off-by: l00913061 <lishihao16@huawei.com>
    Change-Id: Id05a7e8126d9549feac4914d1f6e0f1b2a9412b9

--- arkui-plugins/common/predefines.ts
+++ arkui-plugins/common/predefines.ts
@@ -54,6 +54,7 @@ export enum StructDecoratorNames {
     RESUABLE = 'Reusable',
     RESUABLE_V2 = 'ReusableV2',
     CUSTOM_LAYOUT = 'CustomLayout',
+    CUSTOMDIALOG = 'CustomDialog',
 }
 
 export enum DecoratorNames {
