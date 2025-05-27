0d0d78b7e3b17d56b102aebb932a6f87941cdd78
Cuecuexiaoyu <yuyi51@huawei.com>
2025-05-27 11:19:20 +0800

    fix class property initialization bug
    Signed-off-by: Cuecuexiaoyu <yuyi51@huawei.com>
    Change-Id: I1458a8c26aca2e854fb07e897d4713942161010d

--- arkui-plugins/test/ut/ui-plugins/decorators/link/link-basic-type.test.ts
+++ arkui-plugins/test/ut/ui-plugins/decorators/link/link-basic-type.test.ts
@@ -37,12 +37,12 @@ const parsedTransform: Plugins = {
 };
 
 const expectedScript: string = `
-import { __memo_id_type as __memo_id_type } from "@ohos.arkui.stateManagement";
-import { __memo_context_type as __memo_context_type } from "@ohos.arkui.stateManagement";
-import { memo as memo } from "@ohos.arkui.stateManagement";
+import { __memo_id_type as __memo_id_type } from "arkui.stateManagement.runtime";
+import { __memo_context_type as __memo_context_type } from "arkui.stateManagement.runtime";
+import { memo as memo } from "arkui.stateManagement.runtime";
 import { DecoratedV1VariableBase as DecoratedV1VariableBase } from "@ohos.arkui.stateManagement";
 import { LinkDecoratedVariable as LinkDecoratedVariable } from "@ohos.arkui.stateManagement";
-import { CustomComponent as CustomComponent } from "@ohos.arkui.component";
+import { CustomComponent as CustomComponent } from "arkui.component.customComponent";
 import { Component as Component } from "@ohos.arkui.component";
 import { Link as Link } from "@ohos.arkui.stateManagement";
 
@@ -52,86 +52,86 @@ function main() {}
   public __initializeStruct(initializers: __Options_LinkParent | undefined, @memo() content: (()=> void) | undefined): void {
     if (({let gensym___11910109 = initializers;
     (((gensym___11910109) == (null)) ? undefined : gensym___11910109.__backing_linkVar1)})) {
-      (this).__backing_linkVar1 = new LinkDecoratedVariable<string>("linkVar1", initializers!.__backing_linkVar1!);
+      this.__backing_linkVar1 = new LinkDecoratedVariable<string>("linkVar1", initializers!.__backing_linkVar1!);
     };
     if (({let gensym___181684045 = initializers;
     (((gensym___181684045) == (null)) ? undefined : gensym___181684045.__backing_linkVar2)})) {
-      (this).__backing_linkVar2 = new LinkDecoratedVariable<number>("linkVar2", initializers!.__backing_linkVar2!);
+      this.__backing_linkVar2 = new LinkDecoratedVariable<number>("linkVar2", initializers!.__backing_linkVar2!);
     };
     if (({let gensym___24446313 = initializers;
     (((gensym___24446313) == (null)) ? undefined : gensym___24446313.__backing_linkVar3)})) {
-      (this).__backing_linkVar3 = new LinkDecoratedVariable<boolean>("linkVar3", initializers!.__backing_linkVar3!);
+      this.__backing_linkVar3 = new LinkDecoratedVariable<boolean>("linkVar3", initializers!.__backing_linkVar3!);
     };
     if (({let gensym___167989826 = initializers;
     (((gensym___167989826) == (null)) ? undefined : gensym___167989826.__backing_linkVar4)})) {
-      (this).__backing_linkVar4 = new LinkDecoratedVariable<undefined>("linkVar4", initializers!.__backing_linkVar4!);
+      this.__backing_linkVar4 = new LinkDecoratedVariable<undefined>("linkVar4", initializers!.__backing_linkVar4!);
     };
     if (({let gensym___157566097 = initializers;
     (((gensym___157566097) == (null)) ? undefined : gensym___157566097.__backing_linkVar5)})) {
-      (this).__backing_linkVar5 = new LinkDecoratedVariable<null>("linkVar5", initializers!.__backing_linkVar5!);
+      this.__backing_linkVar5 = new LinkDecoratedVariable<null>("linkVar5", initializers!.__backing_linkVar5!);
     };
   }
   public __updateStruct(initializers: __Options_LinkParent | undefined): void {}
   private __backing_linkVar1?: LinkDecoratedVariable<string>;
   public get linkVar1(): string {
-    return (this).__backing_linkVar1!.get();
+    return this.__backing_linkVar1!.get();
   }
   public set linkVar1(value: string) {
-    (this).__backing_linkVar1!.set(value);
+    this.__backing_linkVar1!.set(value);
   }
   private __backing_linkVar2?: LinkDecoratedVariable<number>;
   public get linkVar2(): number {
-    return (this).__backing_linkVar2!.get();
+    return this.__backing_linkVar2!.get();
   }
   public set linkVar2(value: number) {
-    (this).__backing_linkVar2!.set(value);
+    this.__backing_linkVar2!.set(value);
   }
   private __backing_linkVar3?: LinkDecoratedVariable<boolean>;
   public get linkVar3(): boolean {
-    return (this).__backing_linkVar3!.get();
+    return this.__backing_linkVar3!.get();
   }
   public set linkVar3(value: boolean) {
-    (this).__backing_linkVar3!.set(value);
+    this.__backing_linkVar3!.set(value);
   }
   private __backing_linkVar4?: LinkDecoratedVariable<undefined>;
   public get linkVar4(): undefined {
-    return (this).__backing_linkVar4!.get();
+    return this.__backing_linkVar4!.get();
   }
   public set linkVar4(value: undefined) {
-    (this).__backing_linkVar4!.set(value);
+    this.__backing_linkVar4!.set(value);
   }
   private __backing_linkVar5?: LinkDecoratedVariable<null>;
   public get linkVar5(): null {
-    return (this).__backing_linkVar5!.get();
+    return this.__backing_linkVar5!.get();
   }
   public set linkVar5(value: null) {
-    (this).__backing_linkVar5!.set(value);
+    this.__backing_linkVar5!.set(value);
   }
   @memo() public _build(@memo() style: ((instance: LinkParent)=> LinkParent) | undefined, @memo() content: (()=> void) | undefined, initializers: __Options_LinkParent | undefined): void {}
   public constructor() {}
 }
 
 interface __Options_LinkParent {
-  abstract set linkVar1(linkVar1: string | undefined)
-  abstract get linkVar1(): string | undefined
-  abstract set __backing_linkVar1(__backing_linkVar1: DecoratedV1VariableBase<string> | undefined)
-  abstract get __backing_linkVar1(): DecoratedV1VariableBase<string> | undefined
-  abstract set linkVar2(linkVar2: number | undefined)
-  abstract get linkVar2(): number | undefined
-  abstract set __backing_linkVar2(__backing_linkVar2: DecoratedV1VariableBase<number> | undefined)
-  abstract get __backing_linkVar2(): DecoratedV1VariableBase<number> | undefined
-  abstract set linkVar3(linkVar3: boolean | undefined)
-  abstract get linkVar3(): boolean | undefined
-  abstract set __backing_linkVar3(__backing_linkVar3: DecoratedV1VariableBase<boolean> | undefined)
-  abstract get __backing_linkVar3(): DecoratedV1VariableBase<boolean> | undefined
-  abstract set linkVar4(linkVar4: undefined | undefined)
-  abstract get linkVar4(): undefined | undefined
-  abstract set __backing_linkVar4(__backing_linkVar4: DecoratedV1VariableBase<undefined> | undefined)
-  abstract get __backing_linkVar4(): DecoratedV1VariableBase<undefined> | undefined
-  abstract set linkVar5(linkVar5: null | undefined)
-  abstract get linkVar5(): null | undefined
-  abstract set __backing_linkVar5(__backing_linkVar5: DecoratedV1VariableBase<null> | undefined)
-  abstract get __backing_linkVar5(): DecoratedV1VariableBase<null> | undefined
+  set linkVar1(linkVar1: string | undefined)
+  get linkVar1(): string | undefined
+  set __backing_linkVar1(__backing_linkVar1: DecoratedV1VariableBase<string> | undefined)
+  get __backing_linkVar1(): DecoratedV1VariableBase<string> | undefined
+  set linkVar2(linkVar2: number | undefined)
+  get linkVar2(): number | undefined
+  set __backing_linkVar2(__backing_linkVar2: DecoratedV1VariableBase<number> | undefined)
+  get __backing_linkVar2(): DecoratedV1VariableBase<number> | undefined
+  set linkVar3(linkVar3: boolean | undefined)
+  get linkVar3(): boolean | undefined
+  set __backing_linkVar3(__backing_linkVar3: DecoratedV1VariableBase<boolean> | undefined)
+  get __backing_linkVar3(): DecoratedV1VariableBase<boolean> | undefined
+  set linkVar4(linkVar4: undefined | undefined)
+  get linkVar4(): undefined | undefined
+  set __backing_linkVar4(__backing_linkVar4: DecoratedV1VariableBase<undefined> | undefined)
+  get __backing_linkVar4(): DecoratedV1VariableBase<undefined> | undefined
+  set linkVar5(linkVar5: null | undefined)
+  get linkVar5(): null | undefined
+  set __backing_linkVar5(__backing_linkVar5: DecoratedV1VariableBase<null> | undefined)
+  get __backing_linkVar5(): DecoratedV1VariableBase<null> | undefined
 }
 `;
 
--- arkui-plugins/test/ut/ui-plugins/decorators/link/link-to-link-prop-state.test.ts
+++ arkui-plugins/test/ut/ui-plugins/decorators/link/link-to-link-prop-state.test.ts
@@ -37,16 +37,16 @@ const parsedTransform: Plugins = {
 };
 
 const expectedScript: string = `
-import { __memo_id_type as __memo_id_type } from "@ohos.arkui.stateManagement";
-import { __memo_context_type as __memo_context_type } from "@ohos.arkui.stateManagement";
-import { memo as memo } from "@ohos.arkui.stateManagement";
+import { __memo_id_type as __memo_id_type } from "arkui.stateManagement.runtime";
+import { __memo_context_type as __memo_context_type } from "arkui.stateManagement.runtime";
+import { memo as memo } from "arkui.stateManagement.runtime";
 import { PropDecoratedVariable as PropDecoratedVariable } from "@ohos.arkui.stateManagement";
 import { StateDecoratedVariable as StateDecoratedVariable } from "@ohos.arkui.stateManagement";
 import { DecoratedV1VariableBase as DecoratedV1VariableBase } from "@ohos.arkui.stateManagement";
 import { LinkDecoratedVariable as LinkDecoratedVariable } from "@ohos.arkui.stateManagement";
-import { TextInputAttribute as TextInputAttribute } from "@ohos.arkui.component";
-import { ColumnAttribute as ColumnAttribute } from "@ohos.arkui.component";
-import { CustomComponent as CustomComponent } from "@ohos.arkui.component";
+import { UITextInputAttribute as UITextInputAttribute } from "@ohos.arkui.component";
+import { UIColumnAttribute as UIColumnAttribute } from "@ohos.arkui.component";
+import { CustomComponent as CustomComponent } from "arkui.component.customComponent";
 import { Component as Component, Column as Column, TextInput as TextInput } from "@ohos.arkui.component";
 import { Link as Link, State as State, Prop as Prop } from "@ohos.arkui.stateManagement";
 
@@ -56,29 +56,29 @@ function main() {}
   public __initializeStruct(initializers: __Options_Parant | undefined, @memo() content: (()=> void) | undefined): void {
     if (({let gensym___10127521 = initializers;
     (((gensym___10127521) == (null)) ? undefined : gensym___10127521.__backing_text1)})) {
-      (this).__backing_text1 = new LinkDecoratedVariable<string>("text1", initializers!.__backing_text1!);
+      this.__backing_text1 = new LinkDecoratedVariable<string>("text1", initializers!.__backing_text1!);
     };
   }
   public __updateStruct(initializers: __Options_Parant | undefined): void {}
   private __backing_text1?: LinkDecoratedVariable<string>;
   public get text1(): string {
-    return (this).__backing_text1!.get();
+    return this.__backing_text1!.get();
   }
   public set text1(value: string) {
-    (this).__backing_text1!.set(value);
+    this.__backing_text1!.set(value);
   }
   @memo() public _build(@memo() style: ((instance: Parant)=> Parant) | undefined, @memo() content: (()=> void) | undefined, initializers: __Options_Parant | undefined): void {
     Column(undefined, undefined, (() => {
       TextInput(undefined, {
-        text: (this).text1,
+        text: this.text1,
       }, undefined);
       Child._instantiateImpl(undefined, (() => {
         return new Child();
       }), ({
-        __backing_childText: (this).__backing_text1,
-        childText2: (this).text1,
-        childText3: (this).text1,
-        childText4: (this).text1,
+        __backing_childText: this.__backing_text1,
+        childText2: this.text1,
+        childText3: this.text1,
+        childText4: this.text1,
       } as __Options_Child), undefined, undefined);
     }));
   }
@@ -89,84 +89,84 @@ function main() {}
   public __initializeStruct(initializers: __Options_Child | undefined, @memo() content: (()=> void) | undefined): void {
     if (({let gensym___161337494 = initializers;
     (((gensym___161337494) == (null)) ? undefined : gensym___161337494.__backing_childText)})) {
-      (this).__backing_childText = new LinkDecoratedVariable<string>("childText", initializers!.__backing_childText!);
+      this.__backing_childText = new LinkDecoratedVariable<string>("childText", initializers!.__backing_childText!);
     };
-    (this).__backing_childText2 = new StateDecoratedVariable<string>("childText2", ((({let gensym___95513066 = initializers;
+    this.__backing_childText2 = new StateDecoratedVariable<string>("childText2", ((({let gensym___95513066 = initializers;
     (((gensym___95513066) == (null)) ? undefined : gensym___95513066.childText2)})) ?? ("sss")));
-    (this).__backing_childText3 = new PropDecoratedVariable<string>("childText3", (initializers!.childText3 as string));
-    (this).__backing_childText4 = new PropDecoratedVariable<string>("childText4", ((({let gensym___162028107 = initializers;
+    this.__backing_childText3 = new PropDecoratedVariable<string>("childText3", (initializers!.childText3 as string));
+    this.__backing_childText4 = new PropDecoratedVariable<string>("childText4", ((({let gensym___162028107 = initializers;
     (((gensym___162028107) == (null)) ? undefined : gensym___162028107.childText4)})) ?? ("cc")));
   }
   public __updateStruct(initializers: __Options_Child | undefined): void {
     if (((({let gensym___77632518 = initializers;
     (((gensym___77632518) == (null)) ? undefined : gensym___77632518.childText3)})) !== (undefined))) {
-      (this).__backing_childText3!.update((initializers!.childText3 as string));
+      this.__backing_childText3!.update((initializers!.childText3 as string));
     }
     if (((({let gensym___250510741 = initializers;
     (((gensym___250510741) == (null)) ? undefined : gensym___250510741.childText4)})) !== (undefined))) {
-      (this).__backing_childText4!.update((initializers!.childText4 as string));
+      this.__backing_childText4!.update((initializers!.childText4 as string));
     }
   }
   private __backing_childText?: LinkDecoratedVariable<string>;
   public get childText(): string {
-    return (this).__backing_childText!.get();
+    return this.__backing_childText!.get();
   }
   public set childText(value: string) {
-    (this).__backing_childText!.set(value);
+    this.__backing_childText!.set(value);
   }
   private __backing_childText2?: StateDecoratedVariable<string>;
   public get childText2(): string {
-    return (this).__backing_childText2!.get();
+    return this.__backing_childText2!.get();
   }
   public set childText2(value: string) {
-    (this).__backing_childText2!.set(value);
+    this.__backing_childText2!.set(value);
   }
   private __backing_childText3?: PropDecoratedVariable<string>;
   public get childText3(): string {
-    return (this).__backing_childText3!.get();
+    return this.__backing_childText3!.get();
   }
   public set childText3(value: string) {
-    (this).__backing_childText3!.set(value);
+    this.__backing_childText3!.set(value);
   }
   private __backing_childText4?: PropDecoratedVariable<string>;
   public get childText4(): string {
-    return (this).__backing_childText4!.get();
+    return this.__backing_childText4!.get();
   }
   public set childText4(value: string) {
-    (this).__backing_childText4!.set(value);
+    this.__backing_childText4!.set(value);
   }
   @memo() public _build(@memo() style: ((instance: Child)=> Child) | undefined, @memo() content: (()=> void) | undefined, initializers: __Options_Child | undefined): void {
     TextInput(undefined, {
-      text: (this).childText,
+      text: this.childText,
     }, undefined);
   }
   public constructor() {}
 }
 
 interface __Options_Parant {
-  abstract set text1(text1: string | undefined)
-  abstract get text1(): string | undefined
-  abstract set __backing_text1(__backing_text1: DecoratedV1VariableBase<string> | undefined)
-  abstract get __backing_text1(): DecoratedV1VariableBase<string> | undefined
+  set text1(text1: string | undefined)
+  get text1(): string | undefined
+  set __backing_text1(__backing_text1: DecoratedV1VariableBase<string> | undefined)
+  get __backing_text1(): DecoratedV1VariableBase<string> | undefined
 }
 
 interface __Options_Child {
-  abstract set childText(childText: string | undefined)
-  abstract get childText(): string | undefined
-  abstract set __backing_childText(__backing_childText: DecoratedV1VariableBase<string> | undefined)
-  abstract get __backing_childText(): DecoratedV1VariableBase<string> | undefined
-  abstract set childText2(childText2: string | undefined)
-  abstract get childText2(): string | undefined
-  abstract set __backing_childText2(__backing_childText2: StateDecoratedVariable<string> | undefined)
-  abstract get __backing_childText2(): StateDecoratedVariable<string> | undefined
-  abstract set childText3(childText3: string | undefined)
-  abstract get childText3(): string | undefined
-  abstract set __backing_childText3(__backing_childText3: PropDecoratedVariable<string> | undefined)
-  abstract get __backing_childText3(): PropDecoratedVariable<string> | undefined
-  abstract set childText4(childText4: string | undefined)
-  abstract get childText4(): string | undefined
-  abstract set __backing_childText4(__backing_childText4: PropDecoratedVariable<string> | undefined)
-  abstract get __backing_childText4(): PropDecoratedVariable<string> | undefined
+  set childText(childText: string | undefined)
+  get childText(): string | undefined
+  set __backing_childText(__backing_childText: DecoratedV1VariableBase<string> | undefined)
+  get __backing_childText(): DecoratedV1VariableBase<string> | undefined
+  set childText2(childText2: string | undefined)
+  get childText2(): string | undefined
+  set __backing_childText2(__backing_childText2: StateDecoratedVariable<string> | undefined)
+  get __backing_childText2(): StateDecoratedVariable<string> | undefined
+  set childText3(childText3: string | undefined)
+  get childText3(): string | undefined
+  set __backing_childText3(__backing_childText3: PropDecoratedVariable<string> | undefined)
+  get __backing_childText3(): PropDecoratedVariable<string> | undefined
+  set childText4(childText4: string | undefined)
+  get childText4(): string | undefined
+  set __backing_childText4(__backing_childText4: PropDecoratedVariable<string> | undefined)
+  get __backing_childText4(): PropDecoratedVariable<string> | undefined
 }
 `;
 
