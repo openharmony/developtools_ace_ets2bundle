68a43d352a439f1e8325269dd15045155be3067e
s00912778 <shijiakai2@huawei.com>
2025-07-15 22:18:33 +0800

    support ConditionScope and ConditionBranch
    Signed-off-by: s00912778 <shijiakai2@huawei.com>
    Change-Id: Iac8cbb02051164bd949b217920e249491b1d2b06

---
+++ arkui-plugins/test/ut/ui-plugins/builder-lambda/switch-case-in-content.test.ts
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as path from 'path';
+import { PluginTester } from '../../../utils/plugin-tester';
+import { mockBuildConfig } from '../../../utils/artkts-config';
+import { getRootPath, MOCK_ENTRY_DIR_PATH } from '../../../utils/path-config';
+import { parseDumpSrc } from '../../../utils/parse-string';
+import { memoNoRecheck, recheck, uiNoRecheck } from '../../../utils/plugins';
+import { BuildConfig, PluginTestContext } from '../../../utils/shared-types';
+import { uiTransform } from '../../../../ui-plugins';
+import { Plugins } from '../../../../common/plugin-context';
+
+const BUILDER_LAMBDA_DIR_PATH: string = 'builder-lambda';
+
+const buildConfig: BuildConfig = mockBuildConfig();
+buildConfig.compileFiles = [
+    path.resolve(getRootPath(), MOCK_ENTRY_DIR_PATH, BUILDER_LAMBDA_DIR_PATH, 'switch-case-in-content.ets'),
+];
+
+const pluginTester = new PluginTester('test switch-case conditions in builder lambda call', buildConfig);
+
+const parsedTransform: Plugins = {
+    name: 'switch-case',
+    parsed: uiTransform().parsed,
+};
+
+const expectedUIScript: string = `
+import { ConditionScope as ConditionScope } from \"arkui.component.builder\";
+import { ConditionBranch as ConditionBranch } from \"arkui.component.builder\";
+import { memo as memo } from \"arkui.stateManagement.runtime\";
+import { CustomComponent as CustomComponent } from \"arkui.component.customComponent\";
+import { Text as Text, Column as Column, Component as Component } from \"@ohos.arkui.component\";
+function main() {}
+@Component() final struct SwitchCase extends CustomComponent<SwitchCase, __Options_SwitchCase> {
+    public __initializeStruct(initializers: (__Options_SwitchCase | undefined), @memo() content: ((()=> void) | undefined)): void {
+    this.__backing_num = ((({let gensym___<some_random_number> = initializers;
+    (((gensym___<some_random_number>) == (null)) ? undefined : gensym___<some_random_number>.num)})) ?? (\"1\"));
+    }
+    public __updateStruct(initializers: (__Options_SwitchCase | undefined)): void {}
+    private __backing_num?: string;
+    public get num(): string {
+    return (this.__backing_num as string);
+    }
+    public set num(value: string) {
+    this.__backing_num = value;
+    }
+    @memo() public build() {
+        Column(undefined, undefined, @memo() (() => {
+            ConditionScope(@memo() (() => {
+                switch (this.num) {
+                    case \"0\": {
+                        ConditionBranch(@memo() (() => {
+                            Text(undefined, \"case 0\", undefined, undefined);
+                        }));
+                    }
+                    case \"1\": {
+                        ConditionBranch(@memo() (() => {
+                            Text(undefined, \"case 1\", undefined, undefined);
+                        }));
+                        break;
+                        Text(undefined, \"after break\", undefined, undefined);
+                    }
+                    case \"2\": {
+                        ConditionBranch(@memo() (() => {
+                            Text(undefined, \"case 2\", undefined, undefined);
+                        }));
+                        return;
+                        Text(undefined, \"after return\", undefined, undefined);
+                    }
+                    default: {
+                        ConditionBranch(@memo() (() => {
+                            Text(undefined, \"default\", undefined, undefined);
+                        }));
+                    }
+                }
+            }));
+            Text(undefined, \"hello world\", undefined, undefined);
+        }));
+    }
+    private constructor() {}
+}
+@Component() export interface __Options_SwitchCase {
+    set num(num: (string | undefined))
+    get num(): (string | undefined)
+}
+`;
+
+function testUITransformer(this: PluginTestContext): void {
+    expect(parseDumpSrc(this.scriptSnapshot ?? '')).toBe(parseDumpSrc(expectedUIScript));
+}
+
+const expectedMemoScript: string = `
+import { __memo_context_type as __memo_context_type, __memo_id_type as __memo_id_type } from \"arkui.stateManagement.runtime\";
+import { ConditionScope as ConditionScope } from \"arkui.component.builder\";
+import { ConditionBranch as ConditionBranch } from \"arkui.component.builder\";
+import { memo as memo } from \"arkui.stateManagement.runtime\";
+import { CustomComponent as CustomComponent } from \"arkui.component.customComponent\";
+import { Text as Text, Column as Column, Component as Component } from \"@ohos.arkui.component\";
+function main() {}
+@Component() final struct SwitchCase extends CustomComponent<SwitchCase, __Options_SwitchCase> {
+    public __initializeStruct(initializers: (__Options_SwitchCase | undefined), @memo() content: (((__memo_context: __memo_context_type, __memo_id: __memo_id_type)=> void) | undefined)): void {
+        this.__backing_num = ((({let gensym___<some_random_number> = initializers;
+        (((gensym___<some_random_number>) == (null)) ? undefined : gensym___<some_random_number>.num)})) ?? (\"1\"));
+    }
+    public __updateStruct(initializers: (__Options_SwitchCase | undefined)): void {}
+    private __backing_num?: string;
+    public get num(): string {
+        return (this.__backing_num as string);
+    }
+    public set num(value: string) {
+        this.__backing_num = value;
+    }
+    @memo() public build(__memo_context: __memo_context_type, __memo_id: __memo_id_type) {
+        const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+        if (__memo_scope.unchanged) {
+            __memo_scope.cached;
+            return;
+        }
+        Column(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, undefined, @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+            const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+            if (__memo_scope.unchanged) {
+                __memo_scope.cached;
+                return;
+            }
+            ConditionScope(__memo_context, ((__memo_id) + (<some_random_number>)), @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+                const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+                if (__memo_scope.unchanged) {
+                    __memo_scope.cached;
+                    return;
+                }
+                switch (this.num) {
+                    case \"0\": {
+                        ConditionBranch(__memo_context, ((__memo_id) + (<some_random_number>)), @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+                            const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+                            if (__memo_scope.unchanged) {
+                                __memo_scope.cached;
+                                return;
+                            }
+                            Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"case 0\", undefined, undefined);
+                            {
+                                __memo_scope.recache();
+                                return;
+                            }
+                        }));
+                    }
+                    case \"1\": {
+                        ConditionBranch(__memo_context, ((__memo_id) + (<some_random_number>)), @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+                            const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+                            if (__memo_scope.unchanged) {
+                                __memo_scope.cached;
+                                return;
+                            }
+                            Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"case 1\", undefined, undefined);
+                            {
+                                __memo_scope.recache();
+                                return;
+                            }
+                        }));
+                        break;
+                        Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"after break\", undefined, undefined);
+                    }
+                    case \"2\": {
+                        ConditionBranch(__memo_context, ((__memo_id) + (<some_random_number>)), @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+                            const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+                            if (__memo_scope.unchanged) {
+                                __memo_scope.cached;
+                                return;
+                            }
+                            Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"case 2\", undefined, undefined);
+                            {
+                                __memo_scope.recache();
+                                return;
+                            }
+                        }));
+                        return;
+                        Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"after return\", undefined, undefined);
+                    }
+                    default: {
+                        ConditionBranch(__memo_context, ((__memo_id) + (<some_random_number>)), @memo() ((__memo_context: __memo_context_type, __memo_id: __memo_id_type) => {
+                            const __memo_scope = __memo_context.scope<void>(((__memo_id) + (<some_random_number>)), 0);
+                            if (__memo_scope.unchanged) {
+                                __memo_scope.cached;
+                                return;
+                            }
+                            Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"default\", undefined, undefined);
+                            {
+                                __memo_scope.recache();
+                                return;
+                            }
+                        }));
+                    }
+                }
+                {
+                    __memo_scope.recache();
+                    return;
+                }
+            }));
+            Text(__memo_context, ((__memo_id) + (<some_random_number>)), undefined, \"hello world\", undefined, undefined);
+            {
+                __memo_scope.recache();
+                return;
+            }
+        }));
+        {
+            __memo_scope.recache();
+            return;
+        }
+    }
+    private constructor() {}
+}
+@Component() export interface __Options_SwitchCase {
+    set num(num: (string | undefined))
+    get num(): (string | undefined)
+}
+`;
+
+function testMemoTransformer(this: PluginTestContext): void {
+    expect(parseDumpSrc(this.scriptSnapshot ?? '')).toBe(parseDumpSrc(expectedMemoScript));
+}
+
+pluginTester.run(
+    'test switch-case',
+    [parsedTransform, uiNoRecheck, memoNoRecheck, recheck],
+    {
+        'checked:ui-no-recheck': [testUITransformer],
+        'checked:memo-no-recheck': [testMemoTransformer],
+    },
+    {
+        stopAfter: 'checked',
+    }
+);
