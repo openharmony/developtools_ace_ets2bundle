9154c5045010a93d33065f226409bfc992d53ccd
xieziang <xieziang2@huawei.com>
2025-07-29 21:18:29 +0800

    skip phases
    Signed-off-by: xieziang <xieziang2@huawei.com>
    Change-Id: I903e6ff3608d354ddc49d24e6098f51d4fb8bd09

--- koala-wrapper/native/src/bridges.cc
+++ koala-wrapper/native/src/bridges.cc
@@ -751,6 +751,14 @@ static KNativePointer impl_ProgramSourceFilePathConst(KNativePointer contextPtr,
 }
 KOALA_INTEROP_2(ProgramSourceFilePathConst, KNativePointer, KNativePointer, KNativePointer);
 
+KInt impl_GetCompilationMode(KNativePointer configPtr)
+{
+    auto _config = reinterpret_cast<es2panda_Config *>(configPtr);
+    auto _options = const_cast<es2panda_Options *>(GetImpl()->ConfigGetOptions(_config));
+    return GetImpl()->OptionsUtilGetCompilationModeConst(nullptr, _options);
+}
+KOALA_INTEROP_1(GetCompilationMode, KInt, KNativePointer);
+
 KNativePointer impl_CreateTypeNodeFromTsType(KNativePointer context, KNativePointer nodePtr)
 {
     const auto _context = reinterpret_cast<es2panda_Context*>(context);
