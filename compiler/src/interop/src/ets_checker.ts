c5aeadadb2737d6f2523b7b147980e3eaaee3cf7
liyancheng2 <liyancheng2@huawei.com>
2025-08-22 17:13:03 +0800

    annotation cannot be used in the har where the compilation output is a js file
    Issue: https://gitee.com/openharmony/third_party_typescript/issues/ICU4O8
    
    Signed-off-by: liyancheng2 <liyancheng2@huawei.com>
    Change-Id: I944ef8a59878d3da96e5235f68d99a6f35047eb0

--- compiler/src/interop/src/ets_checker.ts
+++ compiler/src/interop/src/ets_checker.ts
@@ -226,7 +226,8 @@ function setCompilerOptions(resolveModulePaths: string[]): void {
     'compatibleSdkVersionStage': projectConfig.compatibleSdkVersionStage,
     'compatibleSdkVersion': projectConfig.compatibleSdkVersion,
     'skipOhModulesLint': skipOhModulesLint,
-    'mixCompile': mixCompile
+    'mixCompile': mixCompile,
+    'isCompileJsHar': isCompileJsHar(),
   });
   if (projectConfig.compileMode === ESMODULE) {
     Object.assign(compilerOptions, {
@@ -238,6 +239,11 @@ function setCompilerOptions(resolveModulePaths: string[]): void {
     Object.assign(compilerOptions, {'packageManagerType': 'ohpm'});
   }
   readTsBuildInfoFileInCrementalMode(buildInfoPath, projectConfig);
+}
+
+function isCompileJsHar(): boolean {
+  return projectConfig.compileHar && projectConfig.byteCodeHar === false && projectConfig.buildMode === 'Release' &&
+    projectConfig.obfuscationOptions?.selfConfig.ruleOptions.enable && !projectConfig.useTsHar;
 }
 
 function checkArkTSVersion(): void {
