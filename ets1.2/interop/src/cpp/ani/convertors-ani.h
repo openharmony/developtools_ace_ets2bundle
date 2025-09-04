d223e9fbc16da9bc14d2bebf0c1734d3a68bd889
Nikolay Igotti <nikolay.igotti@huawei.com>
2025-09-04 13:37:15 +0300

    Update Signed-off-by: Nikolay Igotti <nikolay.igotti@huawei.com>

--- ets1.2/interop/src/cpp/ani/convertors-ani.h
+++ ets1.2/interop/src/cpp/ani/convertors-ani.h
@@ -12,7 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifdef CONVERTORS_ANI_H
+#ifndef CONVERTORS_ANI_H
+#define CONVERTORS_ANI_H
 
 #ifdef KOALA_ANI
 
