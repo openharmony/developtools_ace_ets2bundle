f3fe978072fc5fec6ecf4457d33634545d79cb33
Korobeinikov Evgeny <korobeinikov.evgeny@huawei.com>
2025-09-12 20:55:56 +0300

    codecheck2
    Signed-off-by: Korobeinikov Evgeny <korobeinikov.evgeny@huawei.com>
    Change-Id: I96ae523832c856200c4deb184b9e7fe0bc2d2a9a

--- ets1.2/interop/src/cpp/DeserializerBase.h
+++ ets1.2/interop/src/cpp/DeserializerBase.h
@@ -541,7 +541,8 @@ inline void WriteToString(std::string* result, InteropFloat32 value)
     result->append(buf);
 #else
     std::string storage;
-    storage.resize(20);
+    constexpr auto SIZE{20};
+    storage.resize(SIZE);
     // We use to_chars() to avoid locale issues.
     auto rc = std::to_chars(storage.data(), storage.data() + storage.size(), value);
     storage.resize(rc.ptr - storage.data());
