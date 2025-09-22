261a0fb67e105aa01308fdc142df7ebf4a8b0094
wangxinyu11 <wangxinyu168@huawei.com>
2025-09-22 16:21:30 +0800

    详细输出编译报错原因
    Signed-off-by: wangxinyu11 <wangxinyu168@huawei.com>

--- compiler/src/fast_build/ark_compiler/generate_sourcemap.ts
+++ compiler/src/fast_build/ark_compiler/generate_sourcemap.ts
@@ -141,7 +141,9 @@ export class SourceMapGenerator {
       const errInfo: LogData = LogDataFactory.newInstance(
         ErrorCode.ETS2BUNDLE_INTERNAL_GET_MODULE_INFO_FAILED,
         ArkTSInternalErrorDescription,
-        `Failed to get ModuleInfo, moduleId: ${moduleId}`
+        `Failed to get ModuleInfo, moduleId: ${moduleId}`,
+        '',
+        [`Please check in which file this file ${moduleId} was imported.`]
       );
       this.logger.printErrorAndExit(errInfo);
     }
