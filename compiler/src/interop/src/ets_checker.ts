bc2ff3d57d1dee9e894bb4406954ec6dabe27717
ohci1 <ohci1@noreply.gitcode.com>
2025-10-09 21:53:34 +0800

    add runtionOS option to the compilerOptions in order to support kit transform for the path of oh sdk
    Issue: https://gitcode.com/openharmony/third_party_typescript/issues/674
    
    Signed-off-by: liyancheng2 <liyancheng2@huawei.com>
    Change-Id: I0cae90a9ddb87cebede21280f53c365fdea56e18
    
    
    revert merge request: add runtionOS option to the compilerOptions in order to support kit transform for the path of oh sdk
    
    16059495678f96b36b6b85e16528ac756a9caa73
    
    This revert merge request !5537

--- compiler/src/interop/src/ets_checker.ts
+++ compiler/src/interop/src/ets_checker.ts
@@ -237,7 +237,6 @@ function setCompilerOptions(resolveModulePaths: string[]): void {
     'mixCompile': mixCompile,
     'isCompileJsHar': isCompileJsHar(),
     'moduleRootPath': projectConfig.moduleRootPath,
-    'runtimeOS': projectConfig.runtimeOS,
   });
   if (projectConfig.compileMode === ESMODULE) {
     Object.assign(compilerOptions, {
