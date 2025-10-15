46637ffd8b92aa8039955f280dd497dd336faf90
Cuecuexiaoyu <yuyi51@huawei.com>
2025-10-15 11:12:36 +0800

    add AwaitExpression visitor
    Signed-off-by: Cuecuexiaoyu <yuyi51@huawei.com>
    Change-Id: I23e013cf86a8c2599d7b6b61c5bdb81ec70b89ba

--- koala-wrapper/src/arkts-api/index.ts
+++ koala-wrapper/src/arkts-api/index.ts
@@ -73,6 +73,7 @@ export * from '../generated/peers/SwitchStatement';
 export * from '../generated/peers/TSQualifiedName';
 export * from '../generated/peers/ClassStaticBlock';
 export * from '../generated/peers/FunctionExpression';
+export * from '../generated/peers/AwaitExpression';
 
 export * from './types';
 export * from './utilities/private';
