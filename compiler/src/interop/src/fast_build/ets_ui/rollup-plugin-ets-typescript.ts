746e3aa8e317e7fb10450fd7d8687fe665009361
Cuecuexiaoyu <yuyi51@huawei.com>
2025-10-24 15:51:14 +0800

    cherry pick to  interop
    Signed-off-by: Cuecuexiaoyu <yuyi51@huawei.com>
    Change-Id: I00d198c204326bdc8002d347f66e43f0fdd3cc20

--- compiler/src/interop/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
+++ compiler/src/interop/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
@@ -593,7 +593,7 @@ async function processNoTargetSourceFile(id: string, code: string, parentEvent?:
       const newSourceFile: ts.SourceFile = ts.createSourceFile(root.id, root.code, etsCheckerCompilerOptions.target,
         true, undefined, etsCheckerCompilerOptions);
       newSourceFile.originalFileName = newSourceFile.fileName;
-      newSourceFile.resolvePath = root.id;
+      newSourceFile.resolvedPath = root.id;
       newSourceFile.path = root.id;
       globalProgram.program.processImportedModules(newSourceFile, true);
       globalProgram.program.setProgramSourceFiles(newSourceFile);
