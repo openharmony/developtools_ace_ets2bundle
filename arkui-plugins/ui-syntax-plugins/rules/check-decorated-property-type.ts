f340048620e0d2e77dd5535f9854ce15b5add03c
haticeOzdemir <hatice.ozdemir.gurgen@gmail.com>
2025-10-24 09:11:31 +0300

    [reopen] updated rules -- removed builder
    Signed-off-by: haticeOzdemir <hatice.ozdemir.gurgen@gmail.com>
    Change-Id: Ie732d5b184fdf234365f5f637e37b7433d39eaf3

--- arkui-plugins/ui-syntax-plugins/rules/check-decorated-property-type.ts
+++ arkui-plugins/ui-syntax-plugins/rules/check-decorated-property-type.ts
@@ -18,7 +18,7 @@ import { AbstractUISyntaxRule } from './ui-syntax-rule';
 import {
   forbiddenUseStateType,
   getAnnotationUsage, getClassPropertyAnnotationNames, getClassPropertyName,
-  getClassPropertyType, PresetDecorators
+  getClassPropertyType, PresetDecorators,
 } from '../utils';
 
 const forbiddenUseStateTypeForDecorators: string[] = [
@@ -34,47 +34,77 @@ const forbiddenUseStateTypeForDecorators: string[] = [
   PresetDecorators.LOCAL_STORAGE_LINK,
 ];
 
+const forbiddenUseStateTypeForDecoratorsForV2: string[] = [
+  PresetDecorators.BUILDER_PARAM,
+  PresetDecorators.PARAM,
+  PresetDecorators.ONCE,
+  PresetDecorators.LOCAL,
+  PresetDecorators.CONSUME,
+  PresetDecorators.EVENT,
+  PresetDecorators.PROVIDER,
+  PresetDecorators.CONSUMER,
+];
+
 class CheckDecoratedPropertyTypeRule extends AbstractUISyntaxRule {
   public setup(): Record<string, string> {
     return {
       invalidDecoratedPropertyType: `The '@{{decoratorName}}' property '{{propertyName}}' cannot be a '{{propertyType}}' object.`,
+      specifyPropertyType: `The property '{{propertyName}}' must specify a type.`,
     };
   }
 
   public parsed(node: arkts.StructDeclaration): void {
-    if (!arkts.isStructDeclaration(node)) {
+    if (!arkts.isStructDeclaration(node) || !node.definition) {
       return;
     }
-    if (!node.definition) {
-      return;
-    }
+
+    const forbiddenDecorators = this.getForbiddenDecoratorList(node);
+    if (!forbiddenDecorators.length) return;
+
     node.definition.body.forEach(member => {
-      this.checkDecoratedPropertyType(member, forbiddenUseStateTypeForDecorators, forbiddenUseStateType);
+      this.checkDecoratedPropertyType(member, forbiddenDecorators, forbiddenUseStateType);
     });
+  }
+
+  private getForbiddenDecoratorList(node: arkts.StructDeclaration): string[] {
+    const componentV2Decorator = getAnnotationUsage(node, PresetDecorators.COMPONENT_V2);
+    const componentDecorator = getAnnotationUsage(node, PresetDecorators.COMPONENT_V1);
+
+    if (componentV2Decorator) {
+      return forbiddenUseStateTypeForDecoratorsForV2;
+    } else if (componentDecorator) {
+      return forbiddenUseStateTypeForDecorators;
+    }
+
+    return [];
   }
 
   private checkDecoratedPropertyType(
     member: arkts.AstNode,
     annoList: string[],
-    typeList: string[]
+    typeList: string[],
   ): void {
     if (!arkts.isClassProperty(member)) {
       return;
     }
     const propertyName = getClassPropertyName(member);
     const propertyType = getClassPropertyType(member);
-    if (!propertyName || !propertyType) {
-      return;
-    }
     const propertyAnnotationNames: string[] = getClassPropertyAnnotationNames(member);
     const decoratorName: string | undefined =
       propertyAnnotationNames.find((annotation) => annoList.includes(annotation));
-    const isType: boolean = typeList.includes(propertyType);
-    if (!member.key) {
+
+    if (!member.key || !propertyName || !decoratorName) {
       return;
     }
+
     const errorNode = member.key;
-    if (decoratorName && isType) {
+    if (!propertyType) {
+      this.report({
+        node: errorNode,
+        message: this.messages.specifyPropertyType,
+        data: { propertyName },
+      });
+    } else if (typeList.includes(propertyType)) {
       this.report({
         node: errorNode,
         message: this.messages.invalidDecoratedPropertyType,
