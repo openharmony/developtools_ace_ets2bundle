dba61d9ec4976f8c87fc78d372f1a039e94225f3
sniperc96 <chenxi305@huawei.com>
2025-12-31 22:08:50 +0800

    修复parsed阶段BuilderParam尾随闭包初始化报错
    Signed-off-by: sniperc96 <chenxi305@huawei.com>
    Change-Id: If6a0e39c8282ff01c746e9b7b2d69a3a265c1cb4

--- arkui-plugins/ui-syntax-plugins/rules/variable-initialization-via-component-constructor.ts
+++ arkui-plugins/ui-syntax-plugins/rules/variable-initialization-via-component-constructor.ts
@@ -18,11 +18,18 @@ import * as arkts from '@koalaui/libarkts';
 import { FileManager } from '../../common/file-manager';
 import { LANGUAGE_VERSION } from '../../common/predefines';
 
-import { getIdentifierName, getClassPropertyAnnotationNames, PresetDecorators, $_INVOKE, COMPONENT_BUILDER } from '../utils';
+import {
+    getIdentifierName,
+    getAnnotationName,
+    getClassPropertyAnnotationNames,
+    PresetDecorators,
+    $_INVOKE, COMPONENT_BUILDER
+} from '../utils';
 import { AbstractUISyntaxRule } from './ui-syntax-rule';
 
 interface PropertyInitInfo {
     hasRequire: boolean;
+    hasBuilderParam: boolean;
     hasTrailingClosure: boolean;
     shouldInitViaComponentConstructor: boolean;
     cannotInitViaComponentConstructor: boolean;
@@ -30,6 +37,18 @@ interface PropertyInitInfo {
 }
 
 class VariableInitializationViaComponentConstructorRule extends AbstractUISyntaxRule {
+    private mustInitMap: Map<string, Map<string, arkts.ClassProperty>> = new Map();
+    private cannotInitMap: Map<string, Map<string, string>> = new Map();
+
+    private static readonly mustInitInConstructorDecorators: string[][] = [
+        [PresetDecorators.REQUIRE],
+        [PresetDecorators.REQUIRE, PresetDecorators.STATE],
+        [PresetDecorators.REQUIRE, PresetDecorators.PROVIDE],
+        [PresetDecorators.REQUIRE, PresetDecorators.PROP_REF],
+        [PresetDecorators.REQUIRE, PresetDecorators.BUILDER_PARAM],
+        [PresetDecorators.REQUIRE, PresetDecorators.PARAM]
+    ];
+
     private static readonly shouldInitViaComponentConstructor: string[] = [
         PresetDecorators.LINK,
         PresetDecorators.OBJECT_LINK
@@ -49,6 +68,17 @@ class VariableInitializationViaComponentConstructorRule extends AbstractUISyntax
             requireVariableInitializationViaComponentConstructor: `'@Require' decorated '{{varName}}' must be initialized through the component constructor.`,
             disallowVariableInitializationViaComponentConstructor: `The '{{decoratorName}}' property '{{varName}}' in the custom component '{{customComponentName}}' cannot be initialized here (forbidden to specify).`,
         };
+    }
+
+    public beforeTransform(): void {
+        this.mustInitMap = new Map();
+        this.cannotInitMap = new Map();
+    }
+
+    public parsed(node: arkts.AstNode): void {
+        this.initMap(node);
+        this.checkMustInitialize(node);
+        this.checkCannotInitialize(node);
     }
 
     public checked(node: arkts.StructDeclaration): void {
@@ -152,6 +182,7 @@ class VariableInitializationViaComponentConstructorRule extends AbstractUISyntax
 
     private getStructPropertyInfo(member: arkts.ClassProperty, hasTrailingClosure: boolean): PropertyInitInfo {
         let hasRequire: boolean = false;
+        let hasBuilderParam: boolean = false;
         let shouldInitializeViaComponentConstructor: boolean = false;
         let cannotInitViaComponentConstructor: boolean = false;
         let annotationName: string = '';
@@ -159,6 +190,9 @@ class VariableInitializationViaComponentConstructorRule extends AbstractUISyntax
         annotationArray.forEach(annotation => {
             if (annotation === PresetDecorators.REQUIRE) {
                 hasRequire = true;
+            }
+            if (annotation === PresetDecorators.BUILDER_PARAM) {
+                hasBuilderParam = true;
             }
             if (VariableInitializationViaComponentConstructorRule.disallowInitViaComponentConstructor.includes(annotation)) {
                 cannotInitViaComponentConstructor = true;
@@ -170,6 +204,7 @@ class VariableInitializationViaComponentConstructorRule extends AbstractUISyntax
         });
         return {
             hasRequire: hasRequire,
+            hasBuilderParam: hasBuilderParam,
             hasTrailingClosure: hasTrailingClosure,
             shouldInitViaComponentConstructor: shouldInitializeViaComponentConstructor,
             cannotInitViaComponentConstructor: cannotInitViaComponentConstructor,
@@ -177,19 +212,173 @@ class VariableInitializationViaComponentConstructorRule extends AbstractUISyntax
         };
     }
 
-    private getMessageId(propertyInitInfo: PropertyInitInfo, propertyName: string, props: string[]): string | undefined {
+    private getMessageId(info: PropertyInitInfo, propertyName: string, props: string[]): string | undefined {
         let messageId: string | undefined = undefined;
         let hasProp: boolean = props.some(tempProp => tempProp === propertyName);
-        if (propertyInitInfo.hasRequire && !propertyInitInfo.hasTrailingClosure && !hasProp) {
+        if (info.hasRequire && !hasProp && !(info.hasTrailingClosure && info.hasBuilderParam)) {
             messageId = this.messages.requireVariableInitializationViaComponentConstructor;
         }
-        if (propertyInitInfo.shouldInitViaComponentConstructor && !hasProp) {
+        if (info.shouldInitViaComponentConstructor && !hasProp) {
             messageId = this.messages.shouldInitializeViaComponentConstructor;
         }
-        if (propertyInitInfo.cannotInitViaComponentConstructor && hasProp) {
+        if (info.cannotInitViaComponentConstructor && hasProp) {
             messageId = this.messages.disallowVariableInitializationViaComponentConstructor;
         }
         return messageId;
+    }
+
+    // Define a function to add property data to the property map
+    private addPropToMustInit(
+        propertyMap: Map<string, Map<string, arkts.ClassProperty>>,
+        structName: string,
+        propertyName: string,
+        property: arkts.ClassProperty
+    ): void {
+        if (!propertyMap.has(structName)) {
+            propertyMap.set(structName, new Map());
+        }
+        const structProperties = propertyMap.get(structName);
+        if (structProperties) {
+            structProperties.set(propertyName, property);
+        }
+    }
+
+    private addPropToCannotInit(
+        propertyMap: Map<string, Map<string, string>>,
+        structName: string,
+        propertyName: string,
+        annotationName: string
+    ): void {
+        if (!propertyMap.has(structName)) {
+            propertyMap.set(structName, new Map());
+        }
+        const structProperties = propertyMap.get(structName);
+        if (structProperties) {
+            structProperties.set(propertyName, annotationName);
+        }
+    }
+
+    // categorizePropertyBasedOnAnnotations
+    private checkPropertyByAnnotations(item: arkts.AstNode, structName: string): void {
+        if (
+            !arkts.isClassProperty(item) ||
+            !item.key ||
+            !arkts.isIdentifier(item.key) ||
+            item.annotations.length === 0
+        ) {
+            return;
+        }
+        const propertyName: string = item.key.name;
+        if (propertyName === '') {
+            return;
+        }
+        const annotationArray: string[] = getClassPropertyAnnotationNames(item);
+        // If the member variable is decorated, it is added to the corresponding map
+        VariableInitializationViaComponentConstructorRule.mustInitInConstructorDecorators.forEach(arr => {
+            if (arr.every(annotation => annotationArray.includes(annotation))) {
+                this.addPropToMustInit(this.mustInitMap, structName, propertyName, item);
+            }
+        });
+        VariableInitializationViaComponentConstructorRule.disallowInitViaComponentConstructor.forEach(annotation => {
+            if (annotationArray.includes(annotation)) {
+                this.addPropToCannotInit(this.cannotInitMap, structName, propertyName, annotation);
+            }
+        });
+    }
+
+    private initMap(node: arkts.AstNode): void {
+        if (arkts.nodeType(node) !== arkts.Es2pandaAstNodeType.AST_NODE_TYPE_ETS_MODULE) {
+            return;
+        }
+        node.getChildren().forEach((member) => {
+            if (!arkts.isStructDeclaration(member)) {
+                return;
+            }
+            if (!member.definition || !member.definition.ident || !arkts.isIdentifier(member.definition.ident)) {
+                return;
+            }
+            const structName: string = member.definition.ident.name;
+            if (structName === '') {
+                return;
+            }
+            member.definition?.body.forEach((item) => {
+                this.checkPropertyByAnnotations(item, structName);
+            });
+        });
+    }
+
+    private checkMustInitialize(node: arkts.AstNode): void {
+        if (!arkts.isCallExpression(node) || !node.expression) {
+            return;
+        }
+        if (!arkts.isIdentifier(node.expression)) {
+            return;
+        }
+        const structName: string = getIdentifierName(node.expression);
+        if (!this.mustInitMap.has(structName)) {
+            return;
+        }
+        // Get all the properties of a record via StructName
+        const mustInitProperty: Map<string, arkts.ClassProperty> = this.mustInitMap.get(structName)!;
+        const childKeyNameArray: string[] = this.getChildKeyNameArray(node);
+        const hasTrailingClosure = !!node.trailingBlock;
+        // If an attribute that must be initialized is not initialized, an error is reported
+        mustInitProperty.forEach((value, key) => {
+            const hasBuilderParam = value.annotations.some((annotation) => {
+                return getAnnotationName(annotation) === PresetDecorators.BUILDER_PARAM;
+            });
+            if (hasTrailingClosure && hasBuilderParam) {
+                return;
+            }
+            if (!childKeyNameArray.includes(key)) {
+                this.report({
+                    node: node,
+                    message: this.messages.requireVariableInitializationViaComponentConstructor,
+                    data: {
+                        varName: key,
+                    },
+                });
+            }
+        });
+    }
+
+    private checkCannotInitialize(node: arkts.AstNode): void {
+        if (!arkts.isCallExpression(node) || !node.expression) {
+            return;
+        }
+        if (!arkts.isIdentifier(node.expression)) {
+            return;
+        }
+        const structName: string = getIdentifierName(node.expression);
+        if (!this.cannotInitMap.has(structName)) {
+            return;
+        }
+        // Get all the properties of a record via StructName
+        const cannotInitName: Map<string, string> = this.cannotInitMap.get(structName)!;
+        node.arguments.forEach((member) => {
+            member.getChildren().forEach((property) => {
+                if (!arkts.isProperty(property)) {
+                    return;
+                }
+                if (!property.key || !arkts.isIdentifier(property.key)) {
+                    return;
+                }
+                const propertyName = property.key.name;
+                // If a property that cannot be initialized is initialized, an error is reported
+                if (cannotInitName.has(propertyName)) {
+                    const propertyType: string = cannotInitName.get(propertyName)!;
+                    this.report({
+                        node: property,
+                        message: this.messages.disallowVariableInitializationViaComponentConstructor,
+                        data: {
+                            decoratorName: `@${propertyType}`,
+                            varName: propertyName,
+                            customComponentName: structName
+                        },
+                    });
+                }
+            });
+        });
     }
 }
 
