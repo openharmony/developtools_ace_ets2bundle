5ba085b156d5eb2c57d09566912af7365c3d5ae0
Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>
2026-02-09 15:15:06 +0300

    Support long form of optional field syntax
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8388
    
    Tests: use Test-U-Runners and CI
    
    Change-Id: I18705f53590ea4584d18badad527b00a2d7c8fdf
    Signed-off-by: Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>

--- arkui-plugins/test/ut/memo-plugins/function-declarations/internal-memo-arg.test.ts
+++ arkui-plugins/test/ut/memo-plugins/function-declarations/internal-memo-arg.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -140,7 +140,7 @@ export declare interface ControlledScope {
 }
 
 export declare interface StateManager extends StateContext {
-  get node(): (IncrementalNode | undefined)
+  get node(): ((IncrementalNode | undefined) | undefined)
   valueBy<Value>(name: string, global?: boolean): Value
   scopeEx<Value>(id: int, paramCount?: int, create?: (()=> IncrementalNode), compute?: (()=> Value), cleanup?: ((value: (Value | undefined))=> void), once?: boolean, reuseKey?: string): IncrementalScope<Value>
   controlledScope(id: int, invalidate: (()=> void)): ControlledScope
