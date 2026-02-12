4d7c826d2f85668596283e228ed71b46361a7a28
zhangboren <bobo1993324@126.com>
2026-02-12 15:14:01 +0800

    Adds range to getter and setter in Static ArkTS's property translators.
    Signed-off-by: zhangboren<bobo1993324@126.com>

--- arkui-plugins/ui-plugins/property-translators/objectlink.ts
+++ arkui-plugins/ui-plugins/property-translators/objectlink.ts
@@ -106,6 +106,7 @@ export class ObjectLinkTranslator extends PropertyTranslator implements Initiali
         const thisGet: arkts.CallExpression = generateGetOrSetCall(thisValue, GetSetTypes.GET);
         const getter: arkts.MethodDefinition = this.translateGetter(originalName, this.propertyType, thisGet);
         field.range = this.property.range;
+        thisGet.range = this.property.range;
         if (this.isMemoCached) {
             const metadata = findCachedMemoMetadata(this.property, false);
             arkts.NodeCache.getInstance().collect(field, { ...metadata, isWithinTypeParams: true });
