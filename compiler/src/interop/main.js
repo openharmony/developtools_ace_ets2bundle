11bd908ff5563e812f2227f564b8c50dd25619c0
weixin_41569733 <zl_icpc@126.com>
2026-02-12 16:19:17 +0800

    src sync interop
    Signed-off-by: weixin_41569733 <zl_icpc@126.com>

--- compiler/src/interop/main.js
+++ compiler/src/interop/main.js
@@ -651,9 +651,9 @@ function loadBuildJson() {
   if (!!aceBuildJson.byteCodeHar) {
     projectConfig.useTsHar = true;
   }
-  if (!!aceBuildJson.customizedHar) {
-    projectConfig.customizedHar = true;
-  }
+
+  projectConfig.customizedHar = !!aceBuildJson.customizedHar;
+
   if (aceBuildJson.updateVersionInfo) {
     projectConfig.updateVersionInfo = aceBuildJson.updateVersionInfo;
   }
