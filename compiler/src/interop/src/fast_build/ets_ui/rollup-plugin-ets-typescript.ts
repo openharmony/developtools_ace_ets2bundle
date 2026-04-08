24e2a394a5d6d0c58e996e3a586c10d42f9fd46d
nicegema102 <zhangkai366@h-partners.com>
2026-04-08 17:11:27 +0800

    Add dots for the collectReservedNameForObf
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/9762
    Test: arkTest
    Co-Authored-By: None
    
    Signed-off-by: nicegema102 <zhangkai366@h-partners.com>

--- compiler/src/interop/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
+++ compiler/src/interop/src/fast_build/ets_ui/rollup-plugin-ets-typescript.ts
@@ -562,7 +562,7 @@ async function transform(code: string, id: string) {
             expandAllImportPaths(CurrentProcessFile.getChecker(), this, eventEmit),
             processKitImport(id, metaInfo, eventEmit, true, lazyImportOptions),
             collectReservedNameForObf(this.share.arkProjectConfig?.obfuscationMergedObConfig,
-              shouldETSOrTSFileTransformToJSWithoutRemove(id, projectConfig, metaInfo))
+              shouldETSOrTSFileTransformToJSWithoutRemove(id, projectConfig, metaInfo), eventEmit)
           ]
         }
       );
@@ -584,7 +584,7 @@ async function transform(code: string, id: string) {
         expandAllImportPaths(CurrentProcessFile.getChecker(), this, eventTransformNodes),
         processKitImport(id, metaInfo, eventTransformNodes, false, lazyImportOptions),
         collectReservedNameForObf(this.share.arkProjectConfig?.obfuscationMergedObConfig,
-          shouldETSOrTSFileTransformToJSWithoutRemove(id, projectConfig, metaInfo))], false);
+          shouldETSOrTSFileTransformToJSWithoutRemove(id, projectConfig, metaInfo), eventTransformNodes)], false);
       stopEvent(eventTransformNodes);
       MemoryMonitor.stopRecordStage(uiKitrecordInfo);
     }
