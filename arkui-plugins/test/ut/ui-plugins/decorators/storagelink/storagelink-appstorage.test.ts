b1138e755df326b79d2cd0e1f24903205d94a5c4
XuMoheng <xumoheng21@163.com>
2026-05-12 11:55:12 +0800

    adapt static RegisterNamedRouter
    Issue: https://gitcode.com/openharmony/developtools_ace_ets2bundle/issues/4424
    Signed-off-by: xumoheng <xumoheng21@163.com>
    Co-Authored-By: Trae

--- arkui-plugins/test/ut/ui-plugins/decorators/storagelink/storagelink-appstorage.test.ts
+++ arkui-plugins/test/ut/ui-plugins/decorators/storagelink/storagelink-appstorage.test.ts
@@ -156,6 +156,10 @@ class __EntryWrapper extends EntryPoint {
     Index._invoke(undefined, undefined, undefined, undefined, undefined);
   }
   
+  public static RegisterNamedRouter(routerName: string, instance: EntryPoint, param: NavInterface): void {
+    EntryPoint.RegisterNamedRouter(routerName, instance, param);
+  }
+
   public constructor() {}
 }
 
