c293e8439b8362e0fe920e4f250ff224a3e07fb3
Yenan <yenan10@huawei.com>
2026-06-18 17:44:05 +0800

    复用字段整改和黑名单补充 Co-Authored-By: Agent Signed-off-by: Yenan <yenan10@huawei.com>

--- compiler/src/process_component_class.ts
+++ compiler/src/process_component_class.ts
@@ -467,7 +467,7 @@ function createReusePoolPropertyDecl(
   const modifiers = ts.factory.createModifiersFromModifierFlags(
     ts.ModifierFlags.Protected | ts.ModifierFlags.Readonly
   );
-  const name = ts.factory.createIdentifier('___reusePool');
+  const name = ts.factory.createIdentifier('__reusePool__Internal');
 
   const optionsLiteral = ts.factory.createObjectLiteralExpression(
     [
@@ -488,7 +488,7 @@ function createReusePoolPropertyDecl(
 
   const callExpr = ts.factory.createCallExpression(
     ts.factory.createPropertyAccessExpression(
-      ts.factory.createIdentifier('__ReusePool'),
+      ts.factory.createIdentifier('__ReusePool__Internal'),
       ts.factory.createIdentifier('create')
     ),
     undefined,
